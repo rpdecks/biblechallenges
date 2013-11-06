1760430858c5b41e988bdcd4acc5fbaf12859564
Philip Bradley <pdbradley@gmail.com>
2013-11-06 18:52:00 -0500

    undo parser model in order to go with original search found in LTW

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -2,4 +2,6 @@ class Chapter < ActiveRecord::Base
   # attr_accessible :title, :body
   has_many :chapter_challenges
   has_many :challenges, through: :chapter_challenges
+
+  has_many :bookfrags, primary_key: :book_number, foreign_key: :book_number
 end
