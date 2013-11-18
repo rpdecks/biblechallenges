8f2588ee055370132ac3096171c1e3c73812af92
Esteba Arango <marranoparael31@gmail.com>
2013-11-18 02:09:40 -0500

    Cookies issue fixed.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,8 @@ Biblechallenge::Application.configure do
   Biblechallenge::Application.config.session_store :cookie_store, key: 'bible_application_devise_session', domain: ".lvh.me", tld_length: 2
 
 
+  config.middleware.use "CustomDomainCookie", ".lvh.me"
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Biblechallenge::Application.config.session_store :cookie_store, key: '_biblechallenge_session'
+Biblechallenge::Application.config.session_store :cookie_store, key: '_biblechallenge_session',:domain => :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
