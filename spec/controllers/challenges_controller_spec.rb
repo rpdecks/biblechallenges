00316aac9e9343c93dc9457a0fe447e313ae5ad1
Esteba Arango <marranoparael31@gmail.com>
2013-11-21 18:32:18 -0500

    Cleaning up Specs / Finished.

--- spec/controllers/challenges_controller_spec.rb
+++ spec/controllers/challenges_controller_spec.rb
@@ -12,12 +12,11 @@ describe ChallengesController do
 
   end
 
+  let(:challengeowner) { create(:user) }
+  let(:challenge) { create(:challenge, subdomain: "woot", owner: challengeowner) }
+  let(:subdomainurl) { "http://#{@challenge.subdomain}.lvh.me" }
 
   describe 'Guest access' do
-
-    let(:challengeowner) { create(:user) }
-    let(:challenge) { create(:challenge, subdomain: "woot", owner: challengeowner) }
-    let(:subdomainurl) { "http://#{challenge.subdomain}.lvh.me" }
 
     describe 'GET#index' do
 
@@ -39,31 +38,30 @@ describe ChallengesController do
 
     end
 
-
-    describe 'GET#show' do
-
-      # before do
-      #   request.host = "#{challenge.subdomain}.lvh.me"
-      # end
-
-      # it "locates the requested challenge" do
-      #   get :show
-      #   assigns(:challenge).should eq(challenge)      
-      # end
-      # subject { get :show }
-
-      # it { expect(assigns(:challenge)).to_not be_nil }
-      # it { assigns(:readings).should_not be_nil }
-
-    end
-
   end
 
   describe 'User access' do
-    let(:current_user) { create(:user) }
+
+    let(:current_user) { create(:user) }    
+    let!(:membership) {create(:membership, user: current_user, challenge: challenge)}
 
     before do
       sign_in :user, current_user
+    end
+
+    describe 'GET#index' do
+
+
+      it "collects challenges into @challenges" do
+        get :index
+        expect(assigns(:challenges)).to match_array [challenge]
+      end
+
+      it "renders the :index template" do
+        get :index
+        expect(response).to render_template :index
+      end
+
     end
 
   end
@@ -71,19 +69,6 @@ describe ChallengesController do
 end
 
 # describe ChallengesController, "Actions as a visitor" do
-#   render_views
-
-#   let(:challengeowner) { FactoryGirl.create(:user) }
-#   let(:challenge) { FactoryGirl.create(:challenge, subdomain: "woot", owner: challengeowner) }
-#   let(:subdomainurl) { "http://#{@challenge.subdomain}.lvh.me" }
-
-#   context "on GET to #index" do
-
-#     it "should display a list of all the challenges" do
-#       get :index
-#       assigns(:challenges).should_not be_nil
-#     end
-#   end
 
 #   context "on GET to #show" do
 #     subject { get :show }
@@ -94,26 +79,6 @@ end
 
 #     it { assigns(:challenge).should_not be_nil }
 #     it { assigns(:readings).should_not be_nil }
-#   end
-
-# end
-
-# describe ChallengesController, "Actions as a user" do
-#   render_views
-
-#   let(:current_user) { FactoryGirl.create(:user) }
-#   let(:challengeowner) { FactoryGirl.create(:user) }
-#   let(:challenge) { FactoryGirl.create(:challenge, subdomain: "woot", owner: challengeowner) }
-#   let(:subdomainurl) { "http://#{@challenge.subdomain}.lvh.me" }
-
-#   before { sign_in :user, current_user }
-
-#   context "on GET to #index" do
-
-#     it "should display a list of all the challenges" do
-#       get :index
-#       assigns(:challenges).should_not be_nil
-#     end
 #   end
 
 # end
