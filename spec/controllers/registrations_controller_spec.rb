4db20e7169dd410ac0614ecdc1ad20c72e317a32
Esteba Arango <marranoparael31@gmail.com>
2013-11-23 11:48:20 -0500

    More failing test for edit user profile. (password is required)

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -6,30 +6,44 @@ describe RegistrationsController do
 
   describe 'PUT#update' do
 
-    let(:current_user) { create(:user) }  
+    let(:current_user) { create(:user, password:'loquillo' , password_confirmation:'loquillo') }  
 
     before do
       sign_in :user, current_user
     end
 
-    it 'locates the user' do
-      put :update, user: {first_name:'newone',last_name:'newone',username:'newone'}
-      expect(assigns(:user)).to eq(current_user)      
-    end
+    context 'when password is not required' do
 
-    it 'updates basic information without requiring password' do
-      put :update, user: {first_name:'newone',last_name:'newone',username:'newone'}
-      # puts current_user.inspect
-      # puts response.inspect
-      current_user.reload
-      expect(current_user.first_name).to eq('newone')
-      expect(current_user.last_name).to eq('newone')
-      expect(current_user.username).to eq('newone')
-    end
+      it 'locates the user' do
+        put :update, user: {first_name:'newone',last_name:'newone',username:'newone'}
+        expect(assigns(:user)).to eq(current_user)      
+      end
 
-    # it 'updates basic information without requiring password' do
+      it 'updates basic information' do
+        put :update, user: {first_name:'newone',last_name:'newone',username:'newone'}
+        current_user.reload
+        expect(current_user.first_name).to eq('newone')
+        expect(current_user.last_name).to eq('newone')
+        expect(current_user.username).to eq('newone')
+      end
       
-    # end
+    end
+
+
+    context 'when password is required' do
+
+      it 'locates the user' do
+        put :update, user: {password:'newone' , password_confirmation:'newone', current_password:'loquillo'}
+        expect(assigns(:user)).to eq(current_user)      
+      end
+
+      it 'updates password if password_confirmation is correct' do
+        put :update, user: {password:'newone' , password_confirmation:'newone', current_password:'loquillo'}
+        current_user.reload
+        expect(current_user.valid_password?('newone')).to be_true
+      end
+      
+    end
 
   end
 
