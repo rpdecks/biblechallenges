5e1a2502c9d3ff52986b79f47c2d315695c4e81e
Esteba Arango <marranoparael31@gmail.com>
2013-11-22 22:28:10 -0500

    Fixing specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,27 @@ describe User do
       expect(create(:user)).to be_valid
     end
 
+
+    # context 'when updating' do
+      
+    #   it "is invalid without a user name" do
+    #     membership = create(:membership, username: "")
+    #     expect(membership).to have(1).errors_on(:username)
+    #   end
+
+    #   it "is invalid without a first name" do
+    #     membership = create(:membership, firstname: "")
+    #     expect(membership).to have(1).errors_on(:firstname)
+    #   end
+
+    #   it "is invalid without a last name" do
+    #     membership = create(:membership, lastname: "")
+    #     expect(membership).to have(1).errors_on(:lastname)
+    #   end
+
+    # end
+
+
   end
 
   describe "Relations" do
--- spec/support/creation_macros.rb
+++ spec/support/creation_macros.rb
@@ -5,7 +5,6 @@ module CreationMacros
     fill_in 'challenge_name', with: options[:name]
     fill_in 'challenge_subdomain', with: options[:subdomain]
     fill_in 'challenge_begindate_input', with: options[:begindate]
-    fill_in 'challenge_enddate_input', with: options[:enddate]
     fill_in 'challenge_chapterstoread', with: options[:chapterstoread]
     click_button 'Create Challenge'
   end
