c0f906c65effba18634dbe918211ec240c301655
Esteba Arango <marranoparael31@gmail.com>
2013-11-25 11:03:12 -0500

    [ WIP Card #14] New membership email. 	Some green tests.

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -14,15 +14,28 @@ class Membership < ActiveRecord::Base
 
   attr_accessible :challenge, :user, :bible_version
 
+  # Constants 
   BIBLE_VERSIONS = %w(ASV ESV KJV NASB NKJV)
-
+  
+  # Relations
   belongs_to :user
   belongs_to :challenge
 
+  #  Validations
   validates :challenge_id, presence: true
   validates :bible_version, presence: true
-
   validates_uniqueness_of :user_id, scope: :challenge_id
   validates :bible_version, inclusion: {in: BIBLE_VERSIONS}
 
+  # Callbacks 
+  after_create :successful_creation_email
+
+  private
+
+  # Callbacks
+
+  # - after_create
+  def successful_creation_email
+    MembershipMailer.creation_email(self).deliver
+  end
 end
