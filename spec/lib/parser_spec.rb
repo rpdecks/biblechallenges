6880d398c7300257e3c9c31e406ff79f809f8471
Esteba Arango <marranoparael31@gmail.com>
2013-11-25 07:44:57 -0500

    Moving “parse_query” function where it belongs.

---
+++ spec/lib/parser_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe Parser do
+
+
+  describe "Class methods" do
+
+    describe ".parse_query" do
+
+      it "returns the fragment and chapter of a query" do
+
+        fragment, chapter = Chapter.parse_query("Matt. 5")
+        expect(fragment).to eql "Matt"
+        expect(chapter).to match_array ["5"]
+
+        fragment, chapter = Chapter.parse_query("Mat 5")
+        expect(fragment).to eql "Mat"
+        expect(chapter).to match_array ["5"]
+
+        fragment, chapter = Chapter.parse_query("1 Cor. 8")
+        expect(fragment).to eql "1Cor"
+        expect(chapter).to match_array ["8"]
+
+        fragment, chapter = Chapter.parse_query("Mat. 5")
+        expect(fragment).to eql "Mat"
+        expect(chapter).to match_array ["5"]
+
+        fragment, chapter = Chapter.parse_query("1Co 8")
+        expect(fragment).to eql "1Co"
+        expect(chapter).to match_array ["8"]
+
+        fragment, chapter = Chapter.parse_query("1Co. 8")
+        expect(fragment).to eql "1Co"
+        expect(chapter).to match_array ["8"]
+
+        fragment, chapter = Chapter.parse_query("2 Tim. 8")
+        expect(fragment).to eql "2Tim"
+        expect(chapter).to match_array ["8"]
+      end
+       
+    end
+
+  end
+
+end
