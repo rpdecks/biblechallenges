57d957e7938f462f50cb221ed4f8c4e4e92d1907
Esteba Arango <marranoparael31@gmail.com>
2013-11-26 12:13:41 -0500

    [ WIP Card #12] Create membership when joining challenge.  - Creates membership for NOT logged in user but account already created.

--- app/models/membership_form.rb
+++ app/models/membership_form.rb
@@ -2,8 +2,8 @@ class MembershipForm
   include ActiveModel::Validations
   include ActiveModel::Conversion
 
-  attr_accessor :email, :bible_version, :user
-  validates_presence_of :email
+  attr_accessor :email, :bible_version, :user, :challenge
+  validates_presence_of :email, :challenge
 
 
   def initialize(attributes = [])
@@ -12,9 +12,21 @@ class MembershipForm
     end
   end
 
-
   def subscribe
     self.user = User.find_or_initialize_by_email email
+    if user.new_record?
+      generated_password = Devise.friendly_token.first(8)
+      self.user.password = generated_password
+      self.user.password_confirmation = generated_password
+      # TODO... Implementing user auto creation.
+    else
+      if challenge.membership_for(user).nil?
+        challenge.memberships.create({user: user, bible_version: bible_version})
+      else
+        errors.add(:email, "already registered in this challenge")
+        false
+      end
+    end
   end
 
   def persisted?
