b74441c6fc9c275013bf096d5945d18586dcebb8
Esteba Arango <marranoparael31@gmail.com>
2013-11-26 09:17:15 -0500

    Fin on a mailer subject and proper ‘challenge_status_button’.

---
+++ app/helpers/creator/challenges_helper.rb
@@ -0,0 +1,11 @@
+module Creator::ChallengesHelper
+
+  def challenge_status_button
+    if @challenge.active?
+      "<h3 style='margin-top: 0px;'><span class='label label-success'>Activated</span></h3>".html_safe
+    else
+      button_to 'Activate my Challenge', activate_creator_challenge_path(@challenge), :class => 'btn btn-primary', :method => 'get'
+    end
+  end
+
+end
