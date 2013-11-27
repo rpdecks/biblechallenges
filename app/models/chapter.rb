e63bdcffde6ba7739a73e244a4c08cd5c0bd1553
Philip Bradley <pdbradley@gmail.com>
2013-11-27 08:41:42 -0500

    add relationship to chapter and verse and new column to verse import table

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -28,6 +28,7 @@ class Chapter < ActiveRecord::Base
   # Relations
   has_many :chapter_challenges
   has_many :challenges, through: :chapter_challenges
+  has_many :verses, primary_key: :chapter_index, foreign_key: :chapter_index
 
   # Class methods
   def self.search(query)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131126004635) do
+ActiveRecord::Schema.define(:version => 20131127042447) do
 
   create_table "bookfrags", :force => true do |t|
     t.string   "fragment"
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(:version => 20131126004635) do
     t.integer  "book_id"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
+    t.integer  "chapter_index"
   end
 
 end
