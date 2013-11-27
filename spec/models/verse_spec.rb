0beaea0dd84de761ed02499eeac6f05be5182aca
Philip Bradley <pdbradley@gmail.com>
2013-11-27 12:24:02 -0500

    add by_version to verse model to look up correct bible version with tests

--- spec/models/verse_spec.rb
+++ spec/models/verse_spec.rb
@@ -11,4 +11,20 @@ describe Verse do
   describe "Relations" do
   end
 
+  describe "scopes" do
+    it "returns a specified version when that version exists" do
+      matthew1 = Chapter.find_by_book_id_and_chapter_number(40, 1)
+      verses = matthew1.verses.by_version("NASB")
+      expect(verses.first.version).to eq "NASB"
+    end
+
+    it "returns the default version when the specified version does not exist" do
+      #how do I put this default version into a constant somewhere?
+      matthew1 = Chapter.find_by_book_id_and_chapter_number(40, 1)
+      verses = matthew1.verses.by_version("IMAGINARY")
+      expect(verses.first.version).to eq "ASV"
+    end
+  end
+    
+
 end
