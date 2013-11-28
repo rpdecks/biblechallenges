8b18b9686dc8281b20aeeacae584149556f34ae6
Esteba Arango <marranoparael31@gmail.com>
2013-11-28 13:43:05 -0500

    [ WIP Card #19] Logging readings. / Email FINISHED

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Biblechallenge::Application.configure do
   config.assets.debug = true
 
   #devise options
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'lvh.me:3000' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
