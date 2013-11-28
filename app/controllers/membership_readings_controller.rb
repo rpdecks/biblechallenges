0a8d3604cfd545de4a78bce8720c4e0eba4b6e18
Esteba Arango <marranoparael31@gmail.com>
2013-11-28 11:45:55 -0500

    [ WIP Card #19] Logging readings.

--- app/controllers/membership_readings_controller.rb
+++ app/controllers/membership_readings_controller.rb
@@ -1,15 +1,32 @@
 class MembershipReadingsController < ApplicationController
 
+  before_filter :find_membership_reading
+
+  layout 'reading_logging'
+
   def confirm
-    @hash = params[:hash]
+    if @membership_reading
+      @hash = params[:hash]
+      @user = @membership_reading.membership.user
+      @reading = @membership_reading.reading
+    else
+      flash[:error] = "This confirmation link doesn't exist"
+    end
   end
 
   def log
-    hashids = HashidsGenerator.instance
-    membership_reading_id = hashids.decrypt(params[:hash])
-    @membership_reading = MembershipReading.find_by_id(membership_reading_id)
     @membership_reading.state = 'read'
     @membership_reading.save!
     render_nothing
   end
+
+
+  private
+
+  def find_membership_reading
+    hashids = HashidsGenerator.instance
+    membership_reading_id = hashids.decrypt(params[:hash])
+    @membership_reading = MembershipReading.find_by_id(membership_reading_id)    
+  end
+
 end
--- spec/controllers/membership_readings_controller_spec.rb
+++ spec/controllers/membership_readings_controller_spec.rb
@@ -18,21 +18,45 @@ describe MembershipReadingsController do
     it {expect({put: "#{subdomainurl}/reading/log/#{hash}"}).to route_to(controller: 'membership_readings', action: 'log', hash: hash) }    
   end
 
-
   describe 'GET#confirm' do
-    it "renders the :new template" do
-      get :confirm, hash: hash
-      expect(response).to render_template(:confirm)
-    end    
     context 'with a valid hash' do
 
+      it "renders the :new template" do
+        get :confirm, hash: hash
+        expect(response).to render_template(:confirm)
+      end
+
+      it "finds the membership_reading's user" do
+        get :confirm, hash: hash
+        expect(assigns(:user)).to eql(membership_reading.membership.user)
+      end
+
+      it "finds the membership_reading's reading" do
+        get :confirm, hash: hash
+        expect(assigns(:reading)).to eql(membership_reading.reading)
+      end
+
     end
+
+    context 'with an invalid hash' do
+      let(:hash){'gB0NV05e'}
+
+      it "renders the :new template" do
+        get :confirm, hash: hash
+        expect(response).to render_template(:confirm)
+      end
+
+      it 'sets a proper flash message' do
+        get :confirm, hash: hash
+        should set_the_flash[:error].to("This confirmation link doesn't exist")
+      end
+    end
+
   end
 
   describe 'PUT#log' do
 
     context 'with a valid hash' do
-
       it "finds membership_reading" do
         put :log, hash: hash
         expect(assigns(:membership_reading)).to eql(membership_reading)
@@ -42,7 +66,6 @@ describe MembershipReadingsController do
         put :log, hash: hash
         expect(membership_reading.reload.state).to eql('read')
       end
-
     end
 
   end
