f7eb82935055d7d65001bb9901126b9122201ec8
Esteba Arango <marranoparael31@gmail.com>
2013-11-27 21:56:25 -0500

    Name of the version on Bible select.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,4 +22,9 @@ module ApplicationHelper
     current_domain.slice!("www.")  #this is so ugly and I am ashamed.  remove www if it is in the request
     [subdomain, current_domain, request.port_string].join
   end
+
+  def select_options_for_bible
+    [["ASV (American Standard)",'ASV'],["ESV (English Standard)",'ESV'],["KJV (King James)",'KJV'],["NASB (New American Standard)",'NASB'],["NKJV (New King James)",'NKJV']]
+  end
+
 end
