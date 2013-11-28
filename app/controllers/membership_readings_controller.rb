5b4e4b518f33d4814c08788fdc629746f2b9095d
Esteba Arango <marranoparael31@gmail.com>
2013-11-28 10:42:36 -0500

    [ WIP Card #19] Logging readings. / More meaningful routes

--- app/controllers/membership_readings_controller.rb
+++ app/controllers/membership_readings_controller.rb
@@ -1,6 +1,11 @@
 class MembershipReadingsController < ApplicationController
+
+  def confirm
+    @hash = params[:hash]
+  end
+
   def log
-    hashids = Hashids.new("ReAdInG LoG")
+    hashids = Hashids.new("ReAdInG LoG",8)
     membership_reading_id = hashids.decrypt(params[:hash])
     @membership_reading = MembershipReading.find_by_id(membership_reading_id)
     @membership_reading.state = 'read'
--- spec/controllers/membership_readings_controller_spec.rb
+++ spec/controllers/membership_readings_controller_spec.rb
@@ -1,15 +1,37 @@
 require 'spec_helper'
 
 describe MembershipReadingsController do
+  
+  let(:challenge){create(:challenge, chapterstoread:'mi 1-4')}
+  let(:user){create(:user)}
+  let(:membership){challenge.join_new_member(user)}
+  let(:membership_reading){membership.membership_readings.first}
+  let(:hash){Hashids.new("ReAdInG LoG",8).encrypt(membership_reading.id)}
 
-  describe 'GET#log' do
+  before do
+    @request.host = "#{challenge.subdomain}.test.com"
+  end
+
+  describe "Routing" do
+    let(:subdomainurl) { "http://#{challenge.subdomain}.test.com" }
+    it {expect({get: "#{subdomainurl}/reading/confirm/#{hash}"}).to route_to(controller: 'membership_readings', action: 'confirm', hash: hash) }
+    it {expect({put: "#{subdomainurl}/reading/log/#{hash}"}).to route_to(controller: 'membership_readings', action: 'log', hash: hash) }    
+  end
+
+
+  describe 'GET#confirm' do
+    it "renders the :new template" do
+      get :confirm, hash: hash
+      expect(response).to render_template(:confirm)
+    end    
+    context 'with a valid hash' do
+
+    end
+  end
+
+  describe 'PUT#log' do
 
     context 'with a valid hash' do
-      let(:challenge){create(:challenge, chapterstoread:'mi 1-4')}
-      let(:user){create(:user)}
-      let(:membership){challenge.join_new_member(user)}
-      let(:membership_reading){membership.membership_readings.first}
-      let(:hash){Hashids.new("ReAdInG LoG").encrypt(membership_reading.id)}
 
       it "finds membership_reading" do
         put :log, hash: hash
