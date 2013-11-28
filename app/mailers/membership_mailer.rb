c502d8dadd71e1946ec9eaa096769d401840d3a8
Esteba Arango <marranoparael31@gmail.com>
2013-11-28 12:15:33 -0500

    [ WIP Card #19] Logging readings. / Refactoring daily emails

--- app/mailers/membership_mailer.rb
+++ app/mailers/membership_mailer.rb
@@ -22,17 +22,4 @@ class MembershipMailer < ActionMailer::Base
       from: "#{@challenge.name.capitalize} <no-reply@#{@challenge.subdomain}.biblechallenges.com>")    
   end
 
-  def dailyreading_email(membership, reading)
-    @membership = membership
-    @chapter = reading.chapter
-    @verses = @chapter.verses.by_version(membership.bible_version)
-    @challenge = @membership.challenge
-    @user = @membership.user
-    @reading = reading
-    mail( to: @user.email, 
-      subject: "#{@challenge.name} reading: #{@chapter.book_name} #{@chapter.chapter_number}", 
-      from: "#{@challenge.name.capitalize} <no-reply@#{@challenge.subdomain}.biblechallenges.com>")
-  end
-
-
 end
