e74ee6b0bf899f98b6ef2c3c9edfd5bea742432b
Esteban Arango Medina <marranoparael31@gmail.com>
2013-12-05 22:01:33 -0500

    [ WIP Card #24] Delete challenge. 	Failing test :).

--- spec/controllers/creator/challenges_controller_spec.rb
+++ spec/controllers/creator/challenges_controller_spec.rb
@@ -1,7 +1,9 @@
-require 'spec_helper' 
+require 'spec_helper'
 require 'creator/challenges_controller'
 
 describe Creator::ChallengesController do
+
+  let(:challenge) {create(:challenge)}
 
   describe 'Routing' do
     it { expect({get: '/creator/challenges/new'}).to route_to(controller: 'creator/challenges', action: 'new') }
@@ -9,22 +11,34 @@ describe Creator::ChallengesController do
   end
 
   describe 'User access' do
-    let(:current_user) { create(:user) }
+    let(:current_user) {create(:user)}
+    let(:challenge) {create(:challenge, owner: current_user)}
 
     before do
       sign_in :user, current_user
     end
-
 
     describe 'GET#new' do
       it "sets up a new empty challenge" do
         get :new
         expect(assigns(:challenge)).to be_a_new(Challenge)
       end
-      
+
       it "renders the :new template" do
         get :new
         expect(response).to render_template(:new)
+      end
+    end
+
+    describe 'GET#edit' do
+      it "finds the right challenge" do
+        get :edit, id: challenge
+        expect(assigns(:challenge)).to eql(challenge)
+      end
+
+      it "renders the :edit template" do
+        get :edit, id: challenge
+        expect(response).to render_template(:edit)
       end
     end
 
@@ -56,7 +70,7 @@ describe Creator::ChallengesController do
           post :create, challenge: attributes_for(:invalid_challenge)
           expect(response).to render_template :new
         end
-        
+
       end
     end
 
@@ -71,10 +85,40 @@ describe Creator::ChallengesController do
       end
 
       it "redirects to the log in page" do
-        get :new 
+        get :new
         expect(response).to redirect_to new_user_session_path
       end
-      
+    end
+
+    describe 'GET#confirm_destroy' do
+      it "does not renders the :new template" do
+        get :confirm_destroy, id: challenge
+        expect(response).to_not render_template(:confirm_destroy)
+      end
+
+      it "redirects to the log in page" do
+        get :confirm_destroy, id: challenge
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe 'DELETE#destroy' do
+      it "redirects to the log in page" do
+        delete :destroy, id: challenge
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe 'GET#edit' do
+      it "does not renders the :new template" do
+        get :edit, id: challenge
+        expect(response).to_not render_template(:edit)
+      end
+
+      it "redirects to the log in page" do
+        get :edit, id: challenge
+        expect(response).to redirect_to new_user_session_path
+      end
     end
 
     describe "POST #create" do
