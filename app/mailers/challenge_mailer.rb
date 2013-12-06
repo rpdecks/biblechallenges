f1df6a7b17b81c9a1edd54b96232eb08b2abc84a
Esteban Arango Medina <marranoparael31@gmail.com>
2013-12-05 22:36:09 -0500

    [ Finished Card #30] Link to the challenge on the creation email

--- app/mailers/challenge_mailer.rb
+++ app/mailers/challenge_mailer.rb
@@ -1,13 +1,15 @@
 class ChallengeMailer < ActionMailer::Base
-  helper ActionView::Helpers::UrlHelper 
+
+  helper ActionView::Helpers::UrlHelper
+
   default from: "Bible Challenges <no-reply@biblechallenges.com>"
   layout 'default_mailer'
 
   # Send an email to the user, when the Challenge creation is succesfull.
   def creation_email(challenge)
     @challenge = challenge
-    @owner = @challenge.owner    
+    @owner = @challenge.owner
     mail( to: @owner.email, subject: "#{@challenge.name} created!")
   end
-  
+
 end
