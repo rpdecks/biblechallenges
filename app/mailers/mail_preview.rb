05efeb1c55791db25a715c0177b1e224578cfa8c
Philip Bradley <pdbradley@gmail.com>
2013-12-18 07:16:07 -0500

    Try to put mailpreview as an only loaded in dev class

--- app/mailers/mail_preview.rb
+++ app/mailers/mail_preview.rb
@@ -1,9 +1,11 @@
-class MailPreview < MailView
+if Rails.env.development?
+  class MailPreview < MailView
 
-  def daily_reading_email
-    challenge = Challenge.first
-    membership_reading = challenge.memberships.first.membership_readings.first
-    MembershipReadingMailer.daily_reading_email(membership_reading)
+    def daily_reading_email
+      challenge = Challenge.first
+      membership_reading = challenge.memberships.first.membership_readings.first
+      MembershipReadingMailer.daily_reading_email(membership_reading)
+    end
+
   end
-
 end
