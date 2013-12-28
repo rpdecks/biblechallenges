5a5f7d212780fcf1bf21b363144267c7722fcdf6
Philip Bradley <pdbradley@gmail.com>
2013-12-28 09:44:13 -0500

    fix mail preview

--- app/mailers/mail_preview.rb
+++ app/mailers/mail_preview.rb
@@ -1,9 +1,27 @@
+require 'securerandom'
+
 if Rails.env.development?
   class MailPreview < MailView
 
     def daily_reading_email
-      challenge = Challenge.first
-      membership_reading = challenge.memberships.first.membership_readings.first
+      user = User.create(
+        username:    Faker::Internet.user_name,
+        first_name:   Faker::Name.first_name,
+        last_name:    Faker::Name.last_name,
+        email:        Faker::Internet.email,
+        password: 'somepassword',
+        password_confirmation: 'somepassword'
+      )
+
+      challenge = Challenge.create(subdomain: SecureRandom.urlsafe_base64(20),
+                                name: Faker::Company.name,
+                                begindate: Time.now,
+                                owner_id: 1, #to pass validations
+                                chapters_to_read: "Matt 1-5")
+
+      membership = Membership.create(challenge: challenge, user: user)
+      binding.pry
+      membership_reading = membership.membership_readings.first
       MembershipReadingMailer.daily_reading_email(membership_reading)
     end
 
