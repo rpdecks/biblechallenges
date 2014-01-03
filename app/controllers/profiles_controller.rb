05718dc0262f4e02aed85d5ef4724659fd4be0e9
Philip Bradley <pdbradley@gmail.com>
2014-01-03 16:12:29 -0500

    move profile fields into profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,15 @@
 class ProfilesController < ApplicationController
-  def update
-  end
 
   def edit
+    @profile = current_user.profile
+  end
+
+  def update
+    @profile = current_user.profile
+    if @profile.update_attributes(params[:profile])
+      flash.now[:notice] = "Profile successfully updated"
+    end
+
+    render :edit 
   end
 end
--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -8,5 +8,48 @@ describe ProfilesController do
   end
 
 
+  context "User Access" do
+
+    describe "GET#edit" do
+      it "assigns the current user's profile to @profile" do
+        current_user = create(:user)
+        sign_in :user, current_user
+        get :edit
+        expect(assigns(:profile)).to eql(current_user.profile)
+      end
+
+      it "renders the :edit template" do
+        current_user = create(:user)
+        sign_in :user, current_user
+        get :edit
+        expect(response).to render_template(:edit)
+      end
+    end
+
+    describe "PUT#update" do
+      context "with valid attributes" do
+        it "updates the profile in the database" do
+          current_user = create(:user)
+          sign_in :user, current_user
+          put :update, profile: attributes_for(:profile, first_name: "Phil",
+                                               last_name: "Bradley",
+                                               username: "yeahbuddy")
+          current_user.reload
+          expect(current_user.first_name).to eql "Phil"
+        end
+      end
+
+        it "renders the profile edit page" do
+          current_user = create(:user)
+          sign_in :user, current_user
+          put :update, profile: attributes_for(:profile, first_name: "Phil",
+                                               last_name: "Bradley",
+                                               username: "yeahbuddy")
+          expect(response).to render_template(:edit)
+        end
+      end
+
+  end
+    
 
 end
