035d8b55fa99f299dc953c85394d43adce14e249
Philip Bradley <pdbradley@gmail.com>
2014-01-07 11:48:11 -0500

    fix feature test to account for activated challenges

--- spec/features/membership_spec.rb
+++ spec/features/membership_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 describe 'Membership creation', type: :feature do
 
   context 'when the user is signed in' do
-    let(:user){create(:user)}
-    let(:challenge){create(:challenge)}
-
-    before do
-      login user
-      visit root_url(subdomain: challenge.subdomain)
-    end
 
     describe 'user joins a challenge' do
       
       it 'joins the challenge' do
+        user = create(:user)
+        challenge = create(:challenge)
+        challenge.active = true
+        challenge.save
+
+        login user
+        visit root_url(subdomain: challenge.subdomain)
         click_button "Join the Challenge"
         page.should have_content 'Thank you for joining!'
       end
@@ -22,16 +22,18 @@ describe 'Membership creation', type: :feature do
   end
 
   context "when the user hasn't signed in" do
-    let(:email){'super_new_email@test.com'}
-    let(:challenge){create(:challenge)}
-
-    before do
-      visit root_url(subdomain: challenge.subdomain)
-    end
 
     describe 'user joins a challenge using only the email' do
       
       it 'joins the challenge' do
+        email = 'super_new_email@test.com'
+        challenge = create(:challenge)
+        challenge.active = true
+        challenge.save
+
+
+        visit root_url(subdomain: challenge.subdomain)
+
         fill_in "membership_form_email", with: email
         click_button "Join the Challenge"        
         page.should have_content 'Thank you for joining, check your email inbox!'
