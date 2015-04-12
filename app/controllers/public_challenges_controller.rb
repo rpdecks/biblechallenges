464d259d0f5c9b0077a31bca245431798aaf2ca2
Philip Bradley <pdbradley@gmail.com>
2015-04-12 07:16:04 -0400

    controller tests passing

--- app/controllers/public_challenges_controller.rb
+++ app/controllers/public_challenges_controller.rb
@@ -1,5 +1,5 @@
 class PublicChallengesController < ApplicationController
   def index
-    @public_challenges = Challenge.public.limit(10)
+    @public_challenges = Challenge.non_private.limit(10)
   end
 end
