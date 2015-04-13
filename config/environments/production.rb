ab954d32b8f4a62ee7fa1b6896385b09296833ee
Philip Bradley <pdbradley@gmail.com>
2015-04-13 08:49:00 -0400

    using secrets.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,9 +74,6 @@ Biblechallenge::Application.configure do
     :domain               => "biblechallenges.com",
     :enable_starttls_auto => true  }
 
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.middleware.use "CustomDomainCookie", ".biblechallenges.com"
 end
