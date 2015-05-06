0a1c8c14264e7644459e0c5b0a8a38563a502d16
Philip Bradley <pdbradley@gmail.com>
2015-05-06 09:19:47 -0400

    fixed deliver to deliver_now

--- app/models/comment_observer.rb
+++ app/models/comment_observer.rb
@@ -2,7 +2,7 @@ class CommentObserver < ActiveRecord::Observer
 
   def after_create(comment)
     if comment.commentable.is_a? Comment
-      CommentMailer.new_comment_notification(comment).deliver
+      CommentMailer.new_comment_notification(comment).deliver_now
     end
   end
 
--- app/models/membership_reading.rb
+++ app/models/membership_reading.rb
@@ -43,7 +43,7 @@ class MembershipReading < ActiveRecord::Base
   def self.send_daily_emails
     MembershipReading.unread.joins(:reading).where("readings.date = ?",Date.today).each do |mr|
       puts "Sending email to: #{mr.membership.user.email} from #{mr.membership.challenge.name} challenge."
-      MembershipReadingMailer.daily_reading_email(mr).deliver
+      MembershipReadingMailer.daily_reading_email(mr).deliver_now
     end
   end
 end
