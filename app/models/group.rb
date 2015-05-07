a830ac0b1719ecf7bc30b46c7bd0d30e4acfa46c
Manuel Garcia <iedidiah@gmail.com>
2015-05-07 16:51:00 -0400

    adding test and code to calcualte the group average punctual reading percentage

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,13 @@ class Group < ActiveRecord::Base
   belongs_to :user
   belongs_to :challenge
   has_many :memberships
+
+
+  def punctual_reading_percentage_average
+    sum_of_member_reading_averages = 0
+    self.memberships.each do |m| 
+      sum_of_member_reading_averages += m.punctual_reading_percentage
+    end
+    sum_of_member_reading_averages / self.memberships.count
+  end
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -7,6 +7,34 @@ RSpec.describe Group, type: :model do
     it { should have_many(:memberships)} 
   end
 
+  describe "#group_punctual_reading_percentage" do
+    it "returns the average percentage of time the readers in the group read according to schedule" do
+        Timecop.travel(4.days.ago)
+        challenge = create(:challenge, chapters_to_read: 'Matt 1-20')
+        membership = create(:membership, challenge: challenge)
+        membership2 = create(:membership, challenge: challenge, user: User.first)
+        group = challenge.groups.create(name: "UC Irvine", user_id: User.first.id)
+        membership.update_attributes(group_id: group.id)
+        membership2.update_attributes(group_id: group.id)
+        membership.membership_readings[0..1].each do |mr| # read first two
+          mr.state = 'read'
+          mr.save!
+        end
+        Timecop.return
+        Timecop.travel(3.days.ago)
+        membership2.membership_readings.first.update_attributes(state: "read")
+        Timecop.return
+        Timecop.travel(2.days.ago)
+        membership2.membership_readings.second.update_attributes(state: "read")
+        Timecop.return
+        Timecop.travel(1.days.ago)
+        membership.membership_readings.third.update_attributes(state: "read")
+        membership2.membership_readings.third.update_attributes(state: "read")
+        Timecop.return
+        expect(group.punctual_reading_percentage_average).to eq 50
+    end
+  end
+
 
 
 end
