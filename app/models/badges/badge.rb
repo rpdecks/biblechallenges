3f36522623f3020956604f22e73751d10dbe3336
Philip Bradley <pdbradley@gmail.com>
2015-05-09 09:03:52 -0400

    refactor badge tests

--- app/models/badges/badge.rb
+++ app/models/badges/badge.rb
@@ -2,23 +2,20 @@ class Badge < ActiveRecord::Base
   belongs_to :user
 
   def self.attach_user_badges(user)
-    # Make sure this user has every badge
-    Badge.descendants.each do |b|
-      unless user.badges.find_by_type(b.name)
-        user.badges << b.create
-      end
-    end
+    # Make sure this user has every badge.
+    # Adjusted so it only makes one query to check instead of one for each badge.
+    # Guy will be happy
+    missing_badges = Badge.descendants.map(&:name) - user.badges.map{|b| b.class.name}
+    missing_badges.each {|mb| user.badges << mb.constantize.create }
   end
 
   def self.update_user_badges(user)
     Badge.attach_user_badges(user)
-
-    user.badges.each do |badge|
-      badge.update
-    end
+    user.badges.each {|badge| badge.update }
   end
 
   def update
+    # this may be overridden in the subclasses of course
     update_attributes(granted: qualify?) 
   end
 end
@@ -27,10 +24,11 @@ end
 
 thought process:
 
-1.  every user needs to have every possible badge associated with him/her
+1.  every user needs to have every possible badge associated with him/her whether they earned it or not
 2.  The granted column shows whether the badge is granted or not
-3.  for every badge a user has, the badge checks whether the user qualifies 
-    and if he does, the badge is granted
+3.  every badge is a subclass of Badge and you can silo the details of each badge in each class
+4.  to add new badges, just create additional badge subclasses according to the pattern
+
 =end
 
 
