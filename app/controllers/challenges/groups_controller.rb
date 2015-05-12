5e1e26859f882e30524592f4e3b4920ccf508e54
Philip Bradley <pdbradley@gmail.com>
2015-05-12 15:31:21 -0400

    removed schema comments from models and refactored groups controller

--- app/controllers/challenges/groups_controller.rb
+++ app/controllers/challenges/groups_controller.rb
@@ -1,21 +1,26 @@
 class Challenges::GroupsController < ApplicationController
 
+  before_filter :find_challenge, :find_group
+
   def show
-    @challenge = Challenge.find(params[:challenge_id])
-    @group = Group.find(params[:id])
   end
 
   def join
-    @challenge = Challenge.find(params[:challenge_id])
-    @group = Group.find(params[:id])
-
     membership = @challenge.membership_for(current_user) || Membership.new(user_id: current_user.id)
     membership.challenge = @challenge
     membership.group = @group
     membership.save
-    
     redirect_to [@challenge, @group]
   end
 
+  private
+
+  def find_challenge
+    @challenge = Challenge.find(params[:challenge_id])
+  end
+
+  def find_group
+    @group = Group.find(params[:id])
+  end
 
 end
