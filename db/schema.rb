5931beb142d5436fd483c53bd0091e62bccb4864
Philip Bradley <pdbradley@gmail.com>
2015-05-13 12:08:24 -0400

    WIP

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508183340) do
+ActiveRecord::Schema.define(version: 20150513160006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "badges", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "type"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "granted",    default: false
+  end
+
+  add_index "badges", ["user_id"], name: "index_badges_on_user_id", using: :btree
 
   create_table "bookfrags", force: :cascade do |t|
     t.string   "fragment"
@@ -84,6 +94,16 @@ ActiveRecord::Schema.define(version: 20150508183340) do
     t.integer  "punctual"
   end
 
+  create_table "membership_statistics", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "type"
+    t.string   "value"
+  end
+
+  add_index "membership_statistics", ["user_id"], name: "index_membership_statistics_on_user_id", using: :btree
+
   create_table "memberships", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "challenge_id"
@@ -116,6 +136,14 @@ ActiveRecord::Schema.define(version: 20150508183340) do
     t.text     "discussion"
   end
 
+  create_table "user_statistics", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "user_statistics", ["user_id"], name: "index_user_statistics_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -129,8 +157,10 @@ ActiveRecord::Schema.define(version: 20150508183340) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "authentication_token"
   end
 
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
@@ -146,4 +176,5 @@ ActiveRecord::Schema.define(version: 20150508183340) do
     t.integer  "chapter_index"
   end
 
+  add_foreign_key "badges", "users"
 end
