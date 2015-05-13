3e4196cfbc76b4bc502ac8a04dcbf96b45495d9b
Philip Bradley <pdbradley@gmail.com>
2015-05-13 18:54:01 -0400

    validations in group specs

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe Group, type: :model do
     it { should belong_to(:user)} 
     it { should have_many(:memberships)} 
   end
+  describe "Validations" do
+    it { should validate_presence_of(:user)} 
+    it { should validate_presence_of(:challenge)} 
+  end
 
 end
