dd3be9395d57c51a5da7d69fb7256dc9d2c6431f
Philip Bradley <pdbradley@gmail.com>
2015-05-14 16:32:48 -0400

    WIP totally broken

--- spec/models/statistics/membership_statistic_record_reading_streak_spec.rb
+++ spec/models/statistics/membership_statistic_record_reading_streak_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe MembershipStatisticRecordSequentialReading do
+describe MembershipStatisticRecordReadingStreak do
   describe "#calculate" do
     it "should calculate the proper value" do
       Timecop.travel(6.days.ago)
