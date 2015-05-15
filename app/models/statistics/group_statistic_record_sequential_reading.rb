35aec3f5c92516a88dc53ccd84a518862d7b422d
Manuel Garcia <iedidiah@gmail.com>
2015-05-15 07:37:32 -0400

    fixing file names

--- app/models/statistics/group_statistic_record_sequential_reading.rb
+++ app/models/statistics/group_statistic_record_sequential_reading.rb
@@ -9,7 +9,7 @@ class GroupStatisticRecordSequentialReading < GroupStatistic
   end
 
   def calculate
-    member_percentages = group.memberships.map {|m| m.membership_statistic_record_sequential_reading.value.to_i}
+    member_percentages = group.memberships.map {|m| m.membership_statistic_record_reading_streak.value.to_i}
     member_percentages.inject{|sum, element| sum + element} / member_percentages.size
   end
 
