f1846111d501a37ae87fb793b8693b5831557467
Evan Koch <evankoch@gmail.com>
2015-05-18 16:02:35 -0400

    basic feature tests to show and list badge types

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,6 +1,11 @@
 class BadgesController < ApplicationController
 
   def index
+    @badge_classes = Badge.descendants
+  end
 
+  def show
+    @badge_class = params[:id].constantize
+    @badge = @badge_class.new
   end
 end
--- spec/features/visitor_views_badges_spec.rb
+++ spec/features/visitor_views_badges_spec.rb
@@ -4,45 +4,32 @@ feature 'Visitor views badges' do
 
   scenario 'Visitor views all badges' do
     visit badges_path
+    expect(page).to have_content("One Chapter Read")
+    expect(page).to have_content("Join Challenge Badge")
   end
+
+  scenario 'Visitor views one badges details' do
+    badge_class = OneChapterBadge
+
+    visit badge_path(id: badge_class.to_s)
+
+    # if we pass in that class name, the show page should
+    # have the name and description of the badge
+    expect(page).to have_content(badge_class.new.name)
+    expect(page).to have_content(badge_class.new.description)
+  end
+
+  scenario 'Visitor clicks on a badge name to see its details' do
+    badge_class = JoinChallengeBadge
+    visit badges_path
+    click_link badge_class.to_s
+
+    # we should be on the show page by now
+    expect(page).to have_content(badge_class.new.name)
+    expect(page).to have_content(badge_class.new.description)
+  end
+
 end
 
-=begin
-  scenario 'User joins a challenge successfully' do
-    challenge = create(:challenge)
-    visit public_challenges_path
-    click_link challenge.name
-    click_link "Join Challenge"
-    expect(challenge.members).to include user
-  end
 
-  scenario 'User should see the Leave Challenge link instead of the Join link if he already in this challenge' do
-    challenge = create(:challenge)
-    create(:membership, challenge: challenge, user: user)
-    visit public_challenges_path
-    click_link challenge.name
 
-    expect(page).to have_content("Leave This Challenge")
-  end
-
-  scenario 'User leaves a challenge successfully' do
-    challenge = create(:challenge)
-    create(:membership, challenge: challenge, user: user)
-    visit public_challenges_path
-    click_link challenge.name
-    click_link "Leave This Challenge"
-    expect(challenge.members).not_to include user
-  end
-
-  scenario 'User should leave his or her group automatically once the user leaves the challenge' do
-    challenge = create(:challenge)
-    group = challenge.groups.create(name: "UC Irvine", user_id: user.id)
-    create(:membership, challenge: challenge, user: user, group_id: group.id)
-    visit public_challenges_path
-    click_link challenge.name
-    click_link "Leave This Challenge"
-
-    expect(challenge.members).not_to include user
-    expect(Membership.count).to eq 0
-  end
-=end
