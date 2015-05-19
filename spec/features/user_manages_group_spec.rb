effe542dea519da4dbfef1bf03a665eafad3344f
Philip Bradley <pdbradley@gmail.com>
2015-05-19 08:07:56 -0400

    model tests for user / membership questions and feature test

--- spec/features/user_manages_group_spec.rb
+++ spec/features/user_manages_group_spec.rb
@@ -40,60 +40,70 @@ feature 'User manages groups' do
       expect(page).to have_content("Test group")
       expect(Group.count).to eq 1
       group = Group.first
-      expect(group.challenge_id).to eq challenge.id
-      expect(group.user_id).to eq user.id
+      expect(group.challenge).to eq challenge
+      expect(group.user).to eq user
       expect(membership.reload.group).to eq group
+    end
+    scenario 'User joins a group successfully' do
+      challenge = create(:challenge)
+      create(:membership, challenge: challenge, user: user)
+      group = challenge.groups.create(name: "UC Irvine", user_id: user.id)
+
+      visit(challenge_path(challenge))
+      click_link "UC Irvine"
+      click_link "Join Group"
+
+      expect(user.groups).to include group
     end
   end
 
-  scenario 'Owner of a group can delete the group with many members in the group' do
-    user1 = create(:user, :with_profile)
-    user2 = create(:user, :with_profile)
-    challenge = create(:challenge)
-    group = challenge.groups.create(user_id: user.id)
-    membership = create(:membership, challenge: challenge, group_id: group.id, user_id: user1.id)
-    membership2 = create(:membership, challenge: challenge, group_id: group.id, user_id: user2.id)
-    visit challenge_group_path(challenge, group)
+  context "Already in a group" do
+    scenario 'Owner of a group can delete the group and will unsubscribe all members in the group' do
+      user1 = create(:user, :with_profile)
+      user2 = create(:user, :with_profile)
+      challenge = create(:challenge)
+      group = challenge.groups.create(user_id: user.id)
+      membership = create(:membership, challenge: challenge, group: group, user: user1)
+      membership2 = create(:membership, challenge: challenge, group: group, user: user2)
+      visit challenge_group_path(challenge, group)
 
-    click_link 'Delete Group'
-    expect(Group.count).to eq 0
-    expect(membership.reload.group_id).to eq nil
-    expect(membership2.reload.group_id).to eq nil
-  end
+      click_link 'Delete Group'
 
+      expect(Group.count).to eq 0
+      expect(membership.reload.group).to eq nil
+      expect(membership2.reload.group).to eq nil
+    end
 
-  scenario 'User joins a group successfully' do
-    challenge = create(:challenge)
-    create(:membership, challenge: challenge, user: user)
-    group = challenge.groups.create(name: "UC Irvine", user_id: user.id)
+    scenario 'User should not see the Create Group link' do
+      #setup
+      challenge = create(:challenge)
+      group1 = challenge.groups.create(name: "UCLA", user_id: user.id)
+      create(:membership, challenge: challenge, user: user, group_id: group1.id)
+      visit(challenge_path(challenge))
+      expect(page).not_to have_link("Create a group")
+    end
 
-    visit(challenge_path(challenge))
-    click_link "UC Irvine"
-    click_link "Join Group"
+    scenario 'User should see the Leave Group link instead of the Join link' do
+      #setup
+      challenge = create(:challenge)
+      group1 = challenge.groups.create(name: "UCLA", user_id: user.id)
+      create(:membership, challenge: challenge, user: user, group_id: group1.id)
+      visit(challenge_path(challenge))
+      click_link group1.name
+      expect(page).not_to have_content("Join Group")
+      expect(page).to have_content("Leave Group")
+    end
 
-    expect(user.groups).to include group
-  end
+    scenario 'User should be able to leaves a group successfully' do
+      challenge = create(:challenge)
+      group = challenge.groups.create(name: "UC Irvine", user_id: user.id)
+      create(:membership, challenge: challenge, user: user, group_id: group.id)
 
-  scenario 'User should see the Leave Group link instead of the Join link if he is already in a group in this challenge' do
-    #setup
-    challenge = create(:challenge)
-    group1 = challenge.groups.create(name: "UCLA", user_id: user.id)
-    create(:membership, challenge: challenge, user: user, group_id: group1.id)
-    visit(challenge_path(challenge))
-    click_link group1.name
-    expect(page).not_to have_content("Join Group")
-    expect(page).to have_content("Leave Group")
-  end
+      visit(challenge_path(challenge))
+      click_link "UC Irvine"
+      click_link "Leave Group"
 
-  scenario 'User leaves a group successfully' do
-    challenge = create(:challenge)
-    group = challenge.groups.create(name: "UC Irvine", user_id: user.id)
-    create(:membership, challenge: challenge, user: user, group_id: group.id)
-
-    visit(challenge_path(challenge))
-    click_link "UC Irvine"
-    click_link "Leave Group"
-
-    expect(user.groups).not_to include group
+      expect(user.groups).not_to include group
+    end
   end
 end
