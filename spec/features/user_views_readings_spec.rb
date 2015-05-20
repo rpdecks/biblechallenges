345f7fe660aeea4064667a570b2124ce6bd0f09f
Philip Bradley <pdbradley@gmail.com>
2015-05-20 13:33:06 -0400

    basic readings feature tests beginning

---
+++ spec/features/user_views_readings_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+feature 'User manages readings' do
+  background do
+    @user = create(:user, :with_profile)
+    @challenge = create(:challenge_with_readings, chapters_to_read: "Matthew 1-2")
+    create(:membership, challenge: @challenge, user: @user)
+    login(@user)
+  end
+
+  context "from the challenge page" do
+    scenario "sees links to the reading pages" do
+      visit challenge_path(@challenge)
+      expect(page).to have_link "Matthew 1"
+    end
+    scenario "clicking reading link takes him to the reading page" do
+      visit challenge_path(@challenge)
+      click_link "Matthew 1"
+      expect(current_path).to eq reading_path(@challenge.readings.first)
+    end
+  end
+    
+
+
+end
