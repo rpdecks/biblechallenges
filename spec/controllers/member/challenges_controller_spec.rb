62f7867cac6f7ba20c4ecd4031e70f740e53fdfe
guy cao <guy.cao@bfa.org>
2015-05-21 12:30:16 -0700

    refactor out creator challenges controller

--- spec/controllers/member/challenges_controller_spec.rb
+++ spec/controllers/member/challenges_controller_spec.rb
@@ -1,9 +1,6 @@
 require 'spec_helper'
-#require 'challenges_controller'
-
 
 describe Member::ChallengesController do
-
   describe "Routing" do
     it { {get: "/member/challenges"}.should route_to(controller: "member/challenges", action: "index") }
   end
@@ -12,9 +9,7 @@ describe Member::ChallengesController do
   let(:challenge) { create(:challenge, owner: challengeowner) }
 
   describe 'Guest access' do
-
     describe 'GET#index' do
-
       it "does not renders the :index template" do
         get :index
         expect(response).to_not render_template(:index)
@@ -25,29 +20,14 @@ describe Member::ChallengesController do
         expect(response).to redirect_to new_user_session_path
       end
     end
-
   end
 
   describe 'User access' do
-
     let(:current_user) { create(:user) }
     let!(:membership) {create(:membership, user: current_user, challenge: challenge)}
 
     before do
       sign_in :user, current_user
-    end
-
-    describe "DELETE#destroy" do
-      it "redirects to the challenges page" do
-        delete :destroy, id: challenge
-        expect(response).to redirect_to member_challenges_url
-      end
-
-      it "removes challenge from database" do
-        expect {
-         delete :destroy, id: challenge
-        }.to change(Challenge, :count).by(-1)
-      end
     end
 
     describe 'GET#index' do
@@ -59,28 +39,6 @@ describe Member::ChallengesController do
       it "renders the :index template" do
         get :index
         expect(response).to render_template :index
-      end
-    end
-
-    describe 'GET#new' do
-      it "sets up a new empty challenge" do
-        get :new
-        expect(assigns(:challenge)).to be_a_new(Challenge)
-      end
-
-      it "renders the :new template" do
-        get :new
-        expect(response).to render_template(:new)
-      end
-    end
-
-    describe "POST #create" do
-      describe "with valid attributes" do
-        it "creates a new challenge" do
-          expect {
-            post :create, challenge: attributes_for(:challenge)
-          }.to change(Challenge, :count).by(1)
-        end
       end
     end
   end
