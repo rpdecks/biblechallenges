3f3a56ab8e97e054d8a63890f0da32afb76dec3c
Philip Bradley <pdbradley@gmail.com>
2015-05-26 13:36:12 -0400

    WIP fixed mailer

--- spec/mailers/reading_mailer_spec.rb
+++ spec/mailers/reading_mailer_spec.rb
@@ -1,15 +1,15 @@
 require "spec_helper"
 
-describe MembershipReadingMailer do
+describe ReadingMailer do
 
   describe '.daily_reading_email' do
     let(:user){create(:user, :with_profile)}
     let(:challenge){create(:challenge_with_readings)}
     let(:membership){challenge.join_new_member(user)}
-    let!(:membership_reading){membership.membership_readings.first}
+    let!(:reading){challenge.readings.first}
 
     it "sends a successful daily email" do
-      expect{ MembershipReadingMailer.daily_reading_email(membership_reading).deliver_now }.to_not raise_error
+      expect{ ReadingMailer.daily_reading_email(reading, membership).deliver_now }.to_not raise_error
     end
 
     it "sends the email to the user's email" do
