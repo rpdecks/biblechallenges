3df7f095ad35aaf6b00a17320588d28dec356877
Philip Bradley <pdbradley@gmail.com>
2015-05-29 10:27:16 -0400

    ajaxy mark read / unread working

--- app/controllers/membership_readings_controller.rb
+++ app/controllers/membership_readings_controller.rb
@@ -12,13 +12,23 @@ class MembershipReadingsController < ApplicationController
     # can I re-render a smaller piece of the page to avoid needing these variables
     # how can I structure this create method so I can hit it from multiple places
     @challenge = membership.challenge
+    reading
     MembershipReading.create(membership_reading_params)
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js { render :create }
+    end
+      
   end
 
   def destroy
-    membership = membership_reading.membership
+    #jim this feel hokey because I want to pass in the id of the membership reading
+    #but since I want to display this link before the actual record exists I'm 
+    # doing it this way
+    membership_reading = current_user.membership_readings.
+      find_by_reading_id_and_membership_id(params[:reading_id], params[:membership_id])
+    reading
     membership_reading.destroy
-    redirect_to [:member, membership]
   end
 
   private
@@ -33,6 +43,10 @@ class MembershipReadingsController < ApplicationController
 
   def membership
     @membership ||= Membership.find(params[:membership_id])
+  end
+
+  def reading
+    @reading ||= Reading.find(params[:reading_id])
   end
 
 end
