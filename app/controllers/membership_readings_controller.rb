8ab65088bdd3a208a9125e6addb32b3a98b34e8a
Philip Bradley <pdbradley@gmail.com>
2015-05-29 00:17:09 -0400

    basic ajaxy membership_reading addition working yay

--- app/controllers/membership_readings_controller.rb
+++ app/controllers/membership_readings_controller.rb
@@ -1,14 +1,18 @@
 class MembershipReadingsController < ApplicationController
 
   before_filter :authenticate_user!
+  respond_to :html, :js
 
   acts_as_token_authentication_handler_for User, only: [:create, :destroy]
 
   layout 'from_email'
 
   def create
+    #jim do I need to instantiate @challenge @reading etc even though I may not need them?
+    # can I re-render a smaller piece of the page to avoid needing these variables
+    # how can I structure this create method so I can hit it from multiple places
+    @challenge = membership.challenge
     MembershipReading.create(membership_reading_params)
-    redirect_to :back
   end
 
   def destroy
