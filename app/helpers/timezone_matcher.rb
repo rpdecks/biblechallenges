1a65cd7efab67bc9416539827f6dd6a1266f58ab
Manuel Garcia <iedidiah@gmail.com>
2015-06-04 15:49:29 -0400

    development code

--- app/helpers/timezone_matcher.rb
+++ app/helpers/timezone_matcher.rb
@@ -1,11 +1,9 @@
 module TimezoneMatcher
-  def timezones_where_the_day_and_hour_are(wday, hour, time = Time.current)
-    binding.pry
+  def self.foo(wday, hour, time = Time.current)
     zones = ActiveSupport::TimeZone.all
     zones.select { |z|
       t = time.in_time_zone(z)
       t.wday == wday && t.hour == hour
     }.map(&:tzinfo).map(&:name)
   end
-
 end
