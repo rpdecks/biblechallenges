380b99c8b1b76d45a08bba285a55759d6f2b745b
Philip Bradley <pdbradley@gmail.com>
2015-06-05 13:46:34 -0400

    wip daily email scheduler

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -46,26 +46,6 @@ class Membership < ActiveRecord::Base
     challenge.readings.count == membership_readings.count
   end
 
-  def self.set_daily_email_jobs
-    Membership.all.each do |m|
-      reading = m.readings.todays_reading.first
-
-      #set time_zone to the user's time_zone
-      user_time_zone = m.user.profile.time_zone
-      Time.zone = user_time_zone
-
-      #convert user_time to UTC
-      user_reading_hour = m.user.profile.preferred_reading_hour
-      user_reading_hour_string = DateTime.now.strftime("%Y-%m-%d") + " " + user_reading_hour.to_s + ":00:00"
-      user_reading_hour_utc = Time.zone.parse(user_reading_hour_string).utc
-
-      #schedule the sidekiq job
-
-      if reading #schedule may be done, but still active
-        DailyEmailWorker.perform_at(user_reading_hour_utc, reading.id, m.user.id)
-      end
-    end
-  end
 
   def associate_statistics
     self.membership_statistics << MembershipStatisticProgressPercentage.create
