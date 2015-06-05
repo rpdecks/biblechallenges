0203703fee3b9017cc9d730d986b56ff0100a1de
Manuel Garcia <iedidiah@gmail.com>
2015-06-05 15:21:53 -0400

    WIP sidekiq test

--- spec/models/service/daily_email_scheduler_spec.rb
+++ spec/models/service/daily_email_scheduler_spec.rb
@@ -11,23 +11,30 @@ describe DailyEmailScheduler do
 
       DailyEmailScheduler.set_daily_email_jobs
 
+        binding.pry
       expect(DailyEmailWorker.jobs.size).to eq 1
 
       Timecop.return
     end
 
     it "schedules an email according to the user's preferences for a user" do
-      include ActiveJob::TestHelper
-      user = create(:user, profile: create(:profile, time_zone: "UTC",
-                                           preferred_reading_hour: 6))
-      challenge = create(:challenge, :with_readings, begindate: "2050-06-01")
-      create(:membership, user: user, challenge: challenge)
+      #include ActiveJob::TestHelper
+      today = Date.now
+      user1 = create(:user, :with_profile) #time_zone: "UTC", preferred_reading_hour: 6
+      user2 = create(:user, profile: create(:profile, time_zone: "EST",
+                                           preferred_reading_hour: 8))
+      challenge = create(:challenge, :with_readings)
+      create(:membership, user: user1, challenge: challenge)
+      create(:membership, user: user2, challenge: challenge)
 
-      Timecop.travel("2050-06-01")
+      t = Time.local(today, 9, 1, 10, 5, 0)
+      Timecop.travel(1.days)
+
       DailyEmailScheduler.set_daily_email_jobs
       binding.pry
-      enqueued_jobs
-      expect(DailyEmailWorker.method :perform).to be_delayed(DailyEmailWorker).for 6.hour
+      #expect(DailyEmailWorker.method :perform).to be_delayed(DailyEmailWorker).for 6.hour
+      
+      Timecop.return
     end
   end
 
