7a1864571a635d21e0b78c8edfcdbeb7e30e5020
Philip Bradley <pdbradley@gmail.com>
2015-06-08 10:02:58 -0400

    massive change from readings date to readings read_on

--- app/models/membership_reading.rb
+++ app/models/membership_reading.rb
@@ -1,6 +1,6 @@
 class MembershipReading < ActiveRecord::Base
   # Scopes
-  default_scope {includes(:reading).order('readings.date')}
+  default_scope {includes(:reading).order('readings.read_on')}
   scope :punctual, -> {where(punctual: 1)}
   scope :not_punctual, -> {where(punctual: 0)}
 
@@ -9,7 +9,7 @@ class MembershipReading < ActiveRecord::Base
   belongs_to :reading
 
   #delegations
-  delegate :date, to: :reading
+  delegate :read_on, to: :reading
 
   # Validations
   validates :membership_id, presence: true
@@ -28,7 +28,7 @@ class MembershipReading < ActiveRecord::Base
   private
 
   def reading_punctual?
-    self.reading.date.to_date === self.updated_at.to_date
+    self.reading.read_on.to_date === self.updated_at.to_date
   end
 
   def mark_punctual
