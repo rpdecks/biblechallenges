e42752a0ed1d0da77853201240f82d13ac4beb8d
Evan Koch <evankoch@gmail.com>
2015-06-09 12:32:20 -0400

    added date blocks/ranges to be evaluated and skipped with challenge readings generator

--- app/models/service/readings_generator.rb
+++ app/models/service/readings_generator.rb
@@ -10,19 +10,41 @@ class ReadingsGenerator
   def initialize(begindate, chapters_to_read, options={})
     @date = begindate
     @chapters_to_read = chapters_to_read
-    @days_of_week_to_skip = options[:days_of_week_to_skip] || []  
-    @date_ranges_to_skip = options[:date_ranges_to_skip] 
+    @days_of_week_to_skip = options[:days_of_week_to_skip] || []
+    @date_ranges_to_skip = options[:date_ranges_to_skip] || []
   end
 
   def generate
+    date_ranges_to_skip = []
     readings = []
     read_on_date = @date
+
+    if @date_ranges_to_skip.present?
+      ranges = @date_ranges_to_skip.first.split(',')
+      ranges.each do |r|
+        beginning = r.split('..').first
+        ending = r.split('..').last
+        begin_date = Date.parse(beginning)
+        end_date = Date.parse(ending)
+        range = begin_date..end_date
+        date_ranges_to_skip << range
+      end
+    end
+
 
     ActsAsScriptural.new.parse(@chapters_to_read).chapters.each do |chapter|
       # if the present day is not included in skippables, create a reading for it
       # Date.wday gives a number betw 0..6 for the day of week (0 is Sunday)
-      while @days_of_week_to_skip.include?(read_on_date.wday) 
-        read_on_date += 1.day
+      if @date_ranges_to_skip.present?
+        date_ranges_to_skip.each do |r|
+          while @days_of_week_to_skip.include?(read_on_date.wday) || r.include?(read_on_date)
+            read_on_date += 1.day
+          end
+        end
+      else
+        while @days_of_week_to_skip.include?(read_on_date.wday)
+          read_on_date += 1.day
+        end
       end
 
       unless @days_of_week_to_skip.include?(read_on_date.wday)
