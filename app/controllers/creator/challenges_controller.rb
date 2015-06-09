1a81a2afe1e9cf09c89a8bf0ebb7a6288be55aad
Philip Bradley <pdbradley@gmail.com>
2015-06-09 15:22:47 -0400

    got dates to skip field in challenge creation form

--- app/controllers/creator/challenges_controller.rb
+++ app/controllers/creator/challenges_controller.rb
@@ -22,8 +22,17 @@ class Creator::ChallengesController < ApplicationController
 
   def create
     @challenge = current_user.created_challenges.build(challenge_params)
+
+    # this seems terrible; is there a better way?  #jim
     flash[:notice] = "Successfully created Challenge" if @challenge.save
-    @challenge.generate_readings
+    readings = ReadingsGenerator.new(@challenge.begindate, 
+                                     @challenge.chapters_to_read,
+                                     {dates_to_skip: challenge_params[:dates_to_skip]}).generate
+    readings.each do |r|
+      r.challenge_id = @challenge.id
+      r.save
+    end
+
     redirect_to [:creator, @challenge]
   end
 
@@ -35,6 +44,6 @@ class Creator::ChallengesController < ApplicationController
   private
 
   def challenge_params
-    params.require(:challenge).permit(:owner_id, :name, :begindate, :enddate, :chapters_to_read)
+    params.require(:challenge).permit(:owner_id, :name, :dates_to_skip, :begindate, :enddate, :chapters_to_read)
   end
 end
