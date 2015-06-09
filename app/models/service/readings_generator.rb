a633f2a6564f7134b0f27d8a37b467d78cabde05
Philip Bradley <pdbradley@gmail.com>
2015-06-09 10:30:30 -0400

    WIP

--- app/models/service/readings_generator.rb
+++ app/models/service/readings_generator.rb
@@ -1,23 +1,42 @@
 class ReadingsGenerator
 
 
-  def initialize(begindate, chapters_to_read)
-    @begindate = begindate
+  # days_of_week_to_skip is an array of days to skip reading.  For example, to skip weekends,
+  # days_of_week_to_skip would be [0,6] because 0 is Sunday and 6 is Saturday
+
+  # date_ranges_to_skip will have the following forms:
+  # "2015-01-01..2015-01-07, 2015-02-01..2015-02-07" 
+
+  def initialize(begindate, chapters_to_read, options={})
+    @date = begindate
     @chapters_to_read = chapters_to_read
+    @days_of_week_to_skip = options[:days_of_week_to_skip] || []  
+    @date_ranges_to_skip = options[:date_ranges_to_skip] 
   end
 
   def generate
     readings = []
-    read_on = @begindate
+    read_on_date = @date
 
     ActsAsScriptural.new.parse(@chapters_to_read).chapters.each do |chapter|
-      chapter = Chapter.find_by_book_id_and_chapter_number(chapter.first, chapter.last)
-      readings << Reading.new(chapter: chapter, read_on: read_on)
-      read_on += 1.day
+      # if the present day is not included in skippables, create a reading for it
+      # Date.wday gives a number betw 0..6 for the day of week (0 is Sunday)
+      while @days_of_week_to_skip.include?(read_on_date.wday) 
+        read_on_date += 1.day
+      end
+
+      unless @days_of_week_to_skip.include?(read_on_date.wday)
+        chapter = Chapter.find_by_book_id_and_chapter_number(chapter.first, chapter.last)
+        readings << Reading.new(chapter: chapter, read_on: read_on_date)
+      end
+
+      read_on_date += 1.day
+
     end
-
-
     readings
   end
+
+
+
 
 end
--- spec/models/service/readings_generator_spec.rb
+++ spec/models/service/readings_generator_spec.rb
@@ -37,6 +37,46 @@ describe ReadingsGenerator do
         expect(readings.last.read_on).to eq Date.parse("2050-01-03")
       end
     end
+
+
+    describe "Skips days of the week correctly" do
+      it "skips the first day of the challenge when the challenge starts on a skip day" do
+        begindate = Date.parse "2050-01-01"  # this is a Saturday
+
+        readings = ReadingsGenerator.new(begindate, "Matthew 1-3", days_of_week_to_skip: [6]).generate
+
+        expect(readings.first.read_on).to eq Date.parse("2050-01-02")
+        expect(readings.size).to eq 3
+      end
+      it "Schedules only on Mondays when we exclude the other days" do
+        begindate = Date.parse "2050-01-01"  # this is a Saturday
+
+        readings = ReadingsGenerator.new(begindate, "Matthew 1-5", days_of_week_to_skip: [0,2,3,4,5,6]).generate
+
+        expect(readings.size).to eq 5
+        readings.each do |r|
+          expect(r.read_on.wday).to eq 1
+        end
+      end
+    end
+
+
+    describe "Skips date ranges correctly" do
+      it "wip" do
+        sample = "2015-01-01..2015-01-07, 2015-02-01..2015-02-07" 
+        ranges = sample.split(',')
+        ranges.each do |r|
+          beginning = r.split('..').first
+          ending = r.split('..').last
+
+
+        end
+
+
+
+
+      end
+    end
   end
 end
 
