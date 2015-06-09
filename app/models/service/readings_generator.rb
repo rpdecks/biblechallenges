3276a109ca2b72e4cf1110defae82c217ba37e95
Philip Bradley <pdbradley@gmail.com>
2015-06-09 14:43:15 -0400

    small refactor of readings generator

--- app/models/service/readings_generator.rb
+++ app/models/service/readings_generator.rb
@@ -11,40 +11,19 @@ class ReadingsGenerator
     @date = begindate
     @chapters_to_read = chapters_to_read
     @days_of_week_to_skip = options[:days_of_week_to_skip] || []
-    @date_ranges_to_skip = options[:date_ranges_to_skip] || []
+    @dates_to_skip = options[:dates_to_skip] || []
   end
 
   def generate
-    date_ranges_to_skip = []
     readings = []
     read_on_date = @date
-
-    if @date_ranges_to_skip.present?
-      ranges = @date_ranges_to_skip.first.split(',')
-      ranges.each do |r|
-        beginning = r.split('..').first
-        ending = r.split('..').last
-        begin_date = Date.parse(beginning)
-        end_date = Date.parse(ending)
-        range = begin_date..end_date
-        date_ranges_to_skip << range
-      end
-    end
-
 
     ActsAsScriptural.new.parse(@chapters_to_read).chapters.each do |chapter|
       # if the present day is not included in skippables, create a reading for it
       # Date.wday gives a number betw 0..6 for the day of week (0 is Sunday)
-      if @date_ranges_to_skip.present?
-        date_ranges_to_skip.each do |r|
-          while @days_of_week_to_skip.include?(read_on_date.wday) || r.include?(read_on_date)
-            read_on_date += 1.day
-          end
-        end
-      else
-        while @days_of_week_to_skip.include?(read_on_date.wday)
-          read_on_date += 1.day
-        end
+
+      while (not_allowed_weekday?(read_on_date) || not_allowed_date?(read_on_date))
+        read_on_date += 1.day
       end
 
       unless @days_of_week_to_skip.include?(read_on_date.wday)
@@ -58,7 +37,37 @@ class ReadingsGenerator
     readings
   end
 
+  private
 
+  def not_allowed_weekday?(a_date)
+    @days_of_week_to_skip.include?(a_date.wday)
+  end
+
+  def not_allowed_date?(a_date)
+    date_ranges_to_skip.any? {|range| range.include?(a_date)}
+  end
+
+  def date_ranges_to_skip
+    ranges_to_skip = []
+    if @dates_to_skip.present?
+      ranges_from_string.each do |range|
+        ranges_to_skip << parse_string_range(range)
+      end
+    end
+    ranges_to_skip
+  end
+
+  def ranges_from_string
+    @dates_to_skip.split(',')
+  end
+
+  def parse_string_range(string_range)
+    beginning = string_range.split('..').first
+    ending = string_range.split('..').last
+    begin_date = Date.parse(beginning)
+    end_date = Date.parse(ending)
+    begin_date..end_date
+  end
 
 
 end
