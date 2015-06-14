8d6ac9c0784143ef807b8026074957d77da1ac0a
Philip Bradley <pdbradley@gmail.com>
2015-06-14 17:58:27 -0400

    moving verses into todays reading

--- app/models/verse.rb
+++ app/models/verse.rb
@@ -1,15 +1,15 @@
 class Verse < ActiveRecord::Base
 
+  Verse::DEFAULT_VERSION = "ASV"
+
   # Relations
   belongs_to :chapter, foreign_key: :chapter_index
 
   default_scope { order(:verse_number) }
 
   def self.by_version(version)
-    #if the chosen version is not in the verses table, go with ASV
-    #this default version should probably be in a constant  (ASV)
     response = where(version: version)
-    response.any? ? response : where(version: "ASV")
+    response.any? ? response : where(version: DEFAULT_VERSION)
   end
 
   def self.by_range(start_verse: nil, end_verse: nil)
--- spec/models/verse_spec.rb
+++ spec/models/verse_spec.rb
@@ -28,24 +28,24 @@ describe Verse do
     end
 
     describe "by_range" do
-      # assuming Genesis 1 is Chapter.first; this may cause problems later.  sorry in advance
+      chapter = Chapter.find_by_book_name_and_chapter_number("Genesis", 1)
       last_verse = 31 # Genesis 1 has 31 verses
 
       it "should return all verses when params are absent" do
-        expect(Chapter.first.verses.by_range.size).to eq last_verse
+        expect(chapter.verses.by_range.size).to eq last_verse
       end
       it "should return the verses in the range if start_verse and end_verse are present" do
-        expect(Chapter.first.verses.by_range(start_verse: 3, end_verse: 5).size).to eq 3
-        expect(Chapter.first.verses.by_range(start_verse: 3, end_verse: 5).first.verse_number).to eq 3
-        expect(Chapter.first.verses.by_range(start_verse: 3, end_verse: 5).last.verse_number).to eq 5
+        expect(chapter.verses.by_range(start_verse: 3, end_verse: 5).size).to eq 3
+        expect(chapter.verses.by_range(start_verse: 3, end_verse: 5).first.verse_number).to eq 3
+        expect(chapter.verses.by_range(start_verse: 3, end_verse: 5).last.verse_number).to eq 5
       end
       it "should return the first X verses up to end_verse if only end_verse is present" do
-        expect(Chapter.first.verses.by_range(end_verse: 5).size).to eq 5
-        expect(Chapter.first.verses.by_range(end_verse: 5).last.verse_number).to eq 5
+        expect(chapter.verses.by_range(end_verse: 5).size).to eq 5
+        expect(chapter.verses.by_range(end_verse: 5).last.verse_number).to eq 5
       end
       it "should return all the verses after start_verse if only start_verse is present" do
-        expect(Chapter.first.verses.by_range(start_verse: 30).size).to eq 2
-        expect(Chapter.first.verses.by_range(start_verse: 30).last.verse_number).to eq last_verse
+        expect(chapter.verses.by_range(start_verse: 30).size).to eq 2
+        expect(chapter.verses.by_range(start_verse: 30).last.verse_number).to eq last_verse
       end
 
     end
