52bc4b23feae283f454125237030e9c81868b053
Philip Bradley <pdbradley@gmail.com>
2015-06-16 16:55:52 -0400

    tests passing

--- spec/factories/challenges.rb
+++ spec/factories/challenges.rb
@@ -7,7 +7,7 @@ FactoryGirl.define do
     name          { Faker::Commerce.product_name + " Challenge"}
     begindate     {Date.today}
     chapters_to_read { ["Matthew 1-2", "John 7-9", "Acts 5-8", "Romans 1-4"].sample }
-    association :owner, factory: :user
+    association :owner, factory: :user_with_profile
 
 
     trait :with_readings do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,8 +27,11 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
+  config.before :suite do
+    DatabaseCleaner.clean
+  end
 
   config.before :each do
     if Capybara.current_driver == :selenium
