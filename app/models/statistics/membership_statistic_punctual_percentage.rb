49938c82b2e38eca47be159da1246c19d4ada7c4
Philip Bradley <pdbradley@gmail.com>
2015-06-16 14:08:10 -0400

    changed punctual to on_schedule throughout

--- app/models/statistics/membership_statistic_punctual_percentage.rb
+++ app/models/statistics/membership_statistic_punctual_percentage.rb
@@ -1,7 +1,7 @@
-class MembershipStatisticPunctualPercentage < MembershipStatistic
+class MembershipStatisticOnSchedulePercentage < MembershipStatistic
 
   def name
-    "Overall punctual  percentage"
+    "Overall on_schedule  percentage"
   end
 
   def description
@@ -10,7 +10,7 @@ class MembershipStatisticPunctualPercentage < MembershipStatistic
 
   def calculate
     td_total = membership.readings.to_date(Date.today).count
-    punct_total = membership.membership_readings.punctual.count
+    punct_total = membership.membership_readings.on_schedule.count
     td_total.zero? ? 0 : (punct_total * 100) / td_total
   end
 
