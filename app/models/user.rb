51b2e9bfee3a2cb94c6b165d457864c78a234df4
guy cao <guy.cao@bfa.org>
2015-06-18 13:59:19 -0700

    update readme with new env key requirements

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
-
+  # @todo: add google+ provider
 
 
   # Relations
