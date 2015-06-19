d4a882b20784c57c314cfc1f2cb0ece4d3415136
guy cao <guy.cao@bfa.org>
2015-06-19 10:51:17 -0700

    configured routes correctly

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,9 +2,21 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
+    #if @user.existing_user?
+    #  sign_in_and_redirect @user
+    #else
+    #  redirect_to new_user_password_path @user
+    #end
+
     if @user.persisted?
-      sign_in_and_redirect @user
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      if @user.existing_user?
+        sign_in_and_redirect @user
+        if is_navigational_format?
+          set_flash_message(:notice, :success, :kind => "Facebook")
+        end
+      else
+        redirect_to finish_signup_path(@user)
+      end
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_path
