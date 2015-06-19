b1c99dd28fb17001f70216875de4cb72629d3066
Manuel Garcia <iedidiah@gmail.com>
2015-06-19 15:21:03 -0400

    fixed failing tests due to changes to reading_mailer

--- app/workers/daily_email_worker.rb
+++ app/workers/daily_email_worker.rb
@@ -3,7 +3,8 @@ class DailyEmailWorker
   #if there is a problem with the email we don't want the worker retrying the job
   sidekiq_options retry: false
 
-    def perform(user_reading_utc, reading, member)
-        ReadingMailer.daily_reading_email(reading, member).deliver_now
-    end
+  def perform(user_reading_utc, reading, member)
+    ReadingMailer.daily_reading_email(reading, member).deliver_now
+  end
+
 end
