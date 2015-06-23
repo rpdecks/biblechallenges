a0c250003e86ca8d9db8d82f9fe972c762524dc0
guy cao <guy.cao@bfa.org>
2015-06-23 11:25:56 -0700

    fix oauth controllers tests

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe Users::OmniauthCallbacksController do
   before do
-    mock_auth_hash
     request.env['devise.mapping'] = Devise.mappings[:user]
   end
 
   describe "#facebook" do
     before do
+      mock_auth_hash("facebook", "ba@example.com")
       request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:facebook]
     end
 
@@ -23,9 +23,10 @@ describe Users::OmniauthCallbacksController do
     end
   end
 
-  describe "#google" do
+  describe "#google_oauth2" do
     before do
-      request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
+      mock_auth_hash("google_oauth2", "pp@example.com")
+      request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
     end
 
     it "should successfully create a user" do
