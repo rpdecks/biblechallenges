53ba3cdd5cc66629534574113e5f2e4ba872f4ec
Evan Koch <evankoch@gmail.com>
2015-06-25 15:08:25 -0400

    migration to add challenge statistic - chapters read

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -9,6 +9,7 @@ class Challenge < ActiveRecord::Base
   has_many :membership_readings, through: :memberships  # needs default order #todo 
   has_many :groups
   has_many :chapters, through: :readings
+  has_many :challenge_statistics
 
   belongs_to :owner, class_name: "User", foreign_key: :owner_id
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619183144) do
+ActiveRecord::Schema.define(version: 20150625183000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 20150619183144) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "book_id"
+  end
+
+  create_table "challenge_statistics", force: :cascade do |t|
+    t.string   "type"
+    t.string   "value"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "challenge_id"
   end
 
   create_table "challenges", force: :cascade do |t|
