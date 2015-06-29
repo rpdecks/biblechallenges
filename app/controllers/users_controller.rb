0c0e7e6db7872b7f348bf97348d4cb24f65ce6d1
guy cao <guy.cao@bfa.org>
2015-06-29 12:08:34 -0700

    test user updates profile

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: "Successfully updated user profile."
+    else
+      flash[:error] = "User not updated. Please try again."
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    accessible = [ :name, :email ]
+    unless params[:user][:password].blank?
+      accessible << [ :password, :password_confirmation ]
+    end
+    params.require(:user).permit(accessible)
+  end
+end
