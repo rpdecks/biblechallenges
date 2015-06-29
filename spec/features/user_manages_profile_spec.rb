fa4c36e01e672ac42bd6a6dd1cb02a78e17e7c88
guy cao <guy.cao@bfa.org>
2015-06-29 12:08:34 -0700

    test user updates profile

--- spec/features/user_manages_profile_spec.rb
+++ spec/features/user_manages_profile_spec.rb
@@ -3,12 +3,14 @@ require 'spec_helper'
 feature 'User manages user profile' do
   let(:user) {create(:user)}
 
-  scenario 'User views own profile successfully' do
-    skip "until we decide what to do with profile page"
-    login(user)
-    visit edit_user_registration_path
-    save_and_open_page
-    expect(find_field('user[name]').value).to eq user.name
+  scenario 'User edits own profile successfully' do
+    login user
+    new_user_params = build(:user)
+    visit edit_user_path user
+
+    fill_in 'user[name]', with: new_user_params.name
+    click_button 'Update User'
+    expect(user.name).to eq new_user_params.name
   end
 
   scenario 'User edits profile succesfully'
