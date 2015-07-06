22fbac9811262f4fb556a2a48f94c282e80aa8fe
guy cao <guy.cao@bfa.org>
2015-07-06 13:35:33 -0700

    update users factory with faker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,10 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :user do
-    email                 {generate(:email)} #todo: refactor this using faker
-    password              "password"
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password "password"
     password_confirmation "password"
-    username    {Faker::Internet.user_name}
+    username { Faker::Internet.user_name }
     time_zone "UTC"
     preferred_reading_hour 6
   end
