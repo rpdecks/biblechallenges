15f046d789ca0c6f1153a406b38a6e59d0abaecc
Philip Bradley <pdbradley@gmail.com>
2015-07-08 16:29:41 -0400

    refresh challenge stats on membership creation

--- app/models/service/membership_completion.rb
+++ app/models/service/membership_completion.rb
@@ -2,7 +2,9 @@ class MembershipCompletion
 
   def initialize(membership)
     MembershipStatisticAttacher.attach_statistics(membership)
-    membership.successful_creation_email
+    membership.update_stats   
+    membership.challenge.update_stats  # this can definitely be backgrounded ask manny how todo
+    membership.successful_creation_email #todo can this be delayed by membership.delay.successful_creation_email
   end
 
 
