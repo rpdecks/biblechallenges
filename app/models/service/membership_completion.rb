d42bbb83cbadc987c79b59c9bfcc8857b99c70c6
Manuel Garcia <iedidiah@gmail.com>
2015-07-09 15:14:10 -0400

    moving the creating membership for challenge creator to controller

--- app/models/service/membership_completion.rb
+++ app/models/service/membership_completion.rb
@@ -3,6 +3,8 @@ class MembershipCompletion
   def initialize(membership)
     MembershipStatisticAttacher.attach_statistics(membership)
     membership.successful_creation_email
+    #todays_readings = membership.readings.todays_reading
+    #ReadingMailer.daily_reading_email(membership.readings.todays_reading.id, membership.user_id)
   end
 
 
