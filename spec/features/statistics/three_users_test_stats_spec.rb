b6714b22be3a13260d8fce4463579e999e3119d4
Philip Bradley <pdbradley@gmail.com>
2015-07-10 14:47:29 -0400

    workaround react checkboxes in a feature test in spec/statistics..not proud of this

--- spec/features/statistics/three_users_test_stats_spec.rb
+++ spec/features/statistics/three_users_test_stats_spec.rb
@@ -4,27 +4,39 @@ feature 'One user reads various parts of a challenge' do
 
   # this is a pure test that only uses browser actions...
 
-  context "One user reads all one chapters in the challenge" do
-    scenario "When joins a group, also joins the challenge" do
+  context "One user reads various amounts of chapters in the challenge" do
+    scenario "Creates a challenge and reads nothing" do
+      #page.driver.block_unknown_urls
 
       create_account_and_log_in(email: 'c@c.com', name: 'C')
-      create_a_challenge(name: "One Day", chapters_to_read: "Mat 1")  #below
-      click_link "One Day"  # title of challenge
-      click_link "Log my reading"
+      create_a_challenge(name: "One Day", chapters_to_read: "Mat 1-2")  #below
 
+      # the challenge stats should exist, and be zeroed
       c = Challenge.first
-      expect(c.challenge_statistic_on_schedule_percentage.value).to eq '0'
-      expect(c.challenge_statistic_chapters_read.value).to eq '1'
-      expect(c.challenge_statistic_progress_percentage.value).to eq '100'
-
       u = User.first
+      m = c.membership_for(u)
+#      expect(c.challenge_statistic_on_schedule_percentage.value).to eq '0'
+#      expect(c.challenge_statistic_chapters_read.value).to eq '0'
+#      expect(c.challenge_statistic_progress_percentage.value).to eq '0'
+
+      reading_one, reading_two = c.readings
+      click_link "One Day"  # title of challenge
+
+      #  This is a terrible thing.  I can't simulate clicking on the checkboxes,
+      #  I think because they are react components.  So simulating the post action
+      #  is as close as I can get to being a "real" user action
+      #  This is clicking one box
+      page.driver.post('/membership_readings.json', { :reading_id => reading_one.id, :membership_id => m.id  }) 
+      page.driver.status_code.should eql 200
+
       expect(u.user_statistic_chapters_read_all_time.value).to eq "1"
       expect(u.user_statistic_days_read_in_a_row_all_time.value).to eq "1"
       expect(u.user_statistic_days_read_in_a_row_current.value).to eq "1"
 
-
     end
   end
+
+
 
 
   def create_account_and_log_in(email: 'somebody@somebody.com', name: 'Somebody')
@@ -33,6 +45,7 @@ feature 'One user reads various parts of a challenge' do
     fill_in "Email", with: email
     fill_in "Password", with: '123123'
     fill_in "Password confirmation", with: '123123'
+
     click_button "Sign up"
   end
 
@@ -41,7 +54,7 @@ feature 'One user reads various parts of a challenge' do
     visit root_path
     click_link "Create a challenge"
     fill_in "Challenge Name", with: name
-    fill_in "challenge[begindate]", with: Date.today
+    fill_in "challenge[begindate]", with: "2017-01-01"
     fill_in "Chapters to Read", with: chapters_to_read
     click_button "Create Challenge"
   end
