d021ce7f094183d62e798a6b4c9c8626af62b3ac
Manuel Garcia <iedidiah@gmail.com>
2015-07-13 15:58:10 -0400

    delaying reading email when schedule is created or member added to challenge

--- spec/models/service/membership_completion_spec.rb
+++ spec/models/service/membership_completion_spec.rb
@@ -6,12 +6,13 @@ describe MembershipCompletion do
   describe "MembershipStatistics" do
     it "should attach user statistics to the user" do
       allow(MembershipStatisticAttacher).to receive(:attach_statistics)
-      ch = create(:challenge, :with_membership)
+      ch = create(:challenge, :with_membership, :with_readings)
       membership = ch.memberships.first
 
       MembershipCompletion.new(membership)
 
-      expect(MembershipStatisticAttacher).to have_received(:attach_statistics).exactly(1).times
+        expect(MembershipStatisticAttacher).to have_received(:attach_statistics).exactly(1).times
+        expect(Sidekiq::Extensions::DelayedMailer.jobs.size).to eq 2
     end
   end
 
