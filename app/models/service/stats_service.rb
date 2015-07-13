00b65f22c9fc8489d240a3b2e59ac4d73a4bea20
Manuel Garcia <iedidiah@gmail.com>
2015-07-13 10:10:39 -0400

    deleting service no longer needed

--- app/models/service/stats_service.rb
+++
@@ -1,9 +0,0 @@
-class StatsService
-
-  def update(membershipID)
-    m = Membership.find(membershipID)
-    m.update_stats
-    m.group.update_stats if m.group
-    m.challenge.update_stats
-  end
-end
