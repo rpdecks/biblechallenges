7b3013320cb98af2b60f0ebe262ad4fae5d22c8b
Manuel Garcia <iedidiah@gmail.com>
2015-07-15 15:12:04 -0400

    moving all emails to after_commit callback

--- app/models/service/membership_completion.rb
+++ app/models/service/membership_completion.rb
@@ -4,11 +4,11 @@ class MembershipCompletion
     MembershipStatisticAttacher.attach_statistics(membership)
 #    membership.successful_creation_email
 
-    todays_reading = membership.readings.todays_reading
-
-    if todays_reading.size > 0
-      DailyEmailWorker.perform_in(1.minute, todays_reading.first.id, membership.user_id)
-    end
+#    todays_reading = membership.readings.todays_reading
+#
+#    if todays_reading.size > 0
+#      DailyEmailWorker.perform_in(1.minute, todays_reading.first.id, membership.user_id)
+#    end
   end
 
 
