9c591915eb5e8f8f282b6298c63bcad2dbc52b0f
Manuel Garcia <iedidiah@gmail.com>
2015-07-15 11:15:18 -0400

    emails need more time to not fail...this is terrible

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -133,7 +133,7 @@ class Challenge < ActiveRecord::Base
   # before save
   # - after_create
   def successful_creation_email
-    NewChallengeEmailWorker.perform_in(5.seconds, self.id)
+    NewChallengeEmailWorker.perform_in(20.seconds, self.id)
   end
 
   # - before_validation
