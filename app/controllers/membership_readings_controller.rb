bb59d1d39b9cb5f3666bfa8b7df3089f6564a100
Manuel Garcia <iedidiah@gmail.com>
2015-07-15 09:54:11 -0400

    issue seems to be timing, so passing in delay of 5 seconds and retry of 5 times

--- app/controllers/membership_readings_controller.rb
+++ app/controllers/membership_readings_controller.rb
@@ -31,7 +31,7 @@ class MembershipReadingsController < ApplicationController
   end
 
   def update_stats
-    UpdateStatsWorker.perform_in(3.seconds, membership.id)
+    UpdateStatsWorker.perform_in(5.seconds, membership.id)
   end
 
   def destroy
