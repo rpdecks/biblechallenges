22b394278024e1dfb54d8360097beeb806264299
guy cao <guy.cao@bfa.org>
2015-07-23 15:35:58 -0400

    fix challenge creation mailer with friendly

--- app/mailers/challenge_mailer.rb
+++ app/mailers/challenge_mailer.rb
@@ -7,7 +7,7 @@ class ChallengeMailer < ActionMailer::Base
 
   # Send an email to the user, when the Challenge creation is succesfull.
   def creation_email(challenge_id)
-    @challenge = Challenge.find(challenge_id)
+    @challenge = Challenge.friendly.find(challenge_id)
     @owner = @challenge.owner
     mail( to: @owner.email, subject: "Bible Challenge #{@challenge.name} created!")
   end
