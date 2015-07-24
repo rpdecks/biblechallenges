b34885069db7bfd28e063d6214bb86f000799ed8
Philip Bradley <pdbradley@gmail.com>
2015-07-24 15:21:48 -0400

    basic tests for schedule calculator

--- app/models/service/chapters_per_date_calculator.rb
+++ app/models/service/chapters_per_date_calculator.rb
@@ -1,16 +1,51 @@
 class ChaptersPerDateCalculator
 
+  attr_reader :schedule
 
-  def initialize(begindate:, num_chapters:, num_chapters_per_day: 1)
+  def initialize(begindate:, num_chapters:, num_chapters_per_day: 1,
+                days_of_week_to_skip: [],
+                date_ranges_to_skip: [])
 
     @begindate = begindate
     @num_chapters= num_chapters
     @num_chapters_per_day = num_chapters_per_day
+    @days_of_week_to_skip = days_of_week_to_skip
+    @date_ranges_to_skip = date_ranges_to_skip
   end
 
 
-  def schedule
-    {}
+
+  def find_available_days
+    result = {}
+    read_on_date = @begindate
+    chapters_left = @num_chapters
+
+    while (chapters_left > 0)
+
+      while not_allowed_date?(read_on_date)
+        read_on_date += 1.day
+      end
+
+      result[read_on_date] = 0
+      chapters_left -= 1
+      read_on_date += 1.day
+    end
+
+    result
+  end
+
+  private
+
+  def not_allowed_date?(a_date)
+    not_allowed_weekday?(a_date) || within_forbidden_date_range?(a_date)
+  end
+
+  def not_allowed_weekday?(a_date)
+    @days_of_week_to_skip.include?(a_date.wday)
+  end
+
+  def within_forbidden_date_range?(a_date)
+    @date_ranges_to_skip.any? {|range| range.include?(a_date)}
   end
 
 
--- spec/models/service/chapters_per_date_calculator_spec.rb
+++ spec/models/service/chapters_per_date_calculator_spec.rb
@@ -7,17 +7,50 @@ describe ChaptersPerDateCalculator do
   # required options passed in are begindate, num_chapters
   # optional are enddate, num_chapters_per_day, dates_to_skip, and days_of_week_to_skip
 
-  describe "With one chapter a day" do
-    it "calculates successfully with no special options" do
-      begindate = Date.parse("2050-01-01")
-      result = ChaptersPerDateCalculator.new(num_chapters: 5, begindate: begindate)
-      binding.pry
+  describe "helper methods" do
+    describe "find_available_days" do
 
-      expect(result.schedule.size).to eq 5
+      it "calculates successfully with no special options" do
+        begindate = Date.parse("2050-01-01")
+        result = ChaptersPerDateCalculator.new(num_chapters: 5, begindate: begindate).find_available_days
 
+        expect(result.size).to eq 5  # 5 unique days
 
-    
+        # should be a hash entry for five days in a row
+        (begindate..Date.parse("2050-01-05")).to_a.each do |a_date|
+          expect(result[a_date]).to_not be_nil
+        end
+      end
 
+      it "calculates successfully with days_to_skip" do
+        begindate = Date.parse("2050-01-01")  # this is a saturday
+        sat_sunday = [6,0]
+        result = ChaptersPerDateCalculator.new(num_chapters: 5, begindate: begindate,
+                                              days_of_week_to_skip: sat_sunday).find_available_days
+
+        expect(result.size).to eq 5  # 5 unique days
+        expect(result[begindate]).to be_nil
+
+        # should be a hash entry for five days in a row starting two days (monday) after begindate
+        ((begindate + 2.days)..Date.parse("2050-01-07")).to_a.each do |a_date|
+          expect(result[a_date]).to_not be_nil
+        end
+      end
+
+      it "calculates successfully with forbidden date ranges" do
+        begindate = Date.parse("2050-01-01")  # this is a saturday
+        skip_range = begindate..(begindate + 2.days)
+        result = ChaptersPerDateCalculator.new(num_chapters: 5, begindate: begindate,
+                                              date_ranges_to_skip: [skip_range]).find_available_days
+
+        expect(result.size).to eq 5  # 5 unique days
+        expect(result[begindate]).to be_nil
+
+        # should be a hash entry for five days in a row starting three days (Tuesday) after begindate
+        ((begindate + 3.days)..Date.parse("2050-01-08")).to_a.each do |a_date|
+          expect(result[a_date]).to_not be_nil
+        end
+      end
 
     end
   end
