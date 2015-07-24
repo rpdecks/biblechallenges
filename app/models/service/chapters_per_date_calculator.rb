766e166638ff76e1b6553d288f132f077c4e2b37
Philip Bradley <pdbradley@gmail.com>
2015-07-24 14:06:15 -0400

    WIP

--- app/models/service/chapters_per_date_calculator.rb
+++ app/models/service/chapters_per_date_calculator.rb
@@ -1,8 +1,19 @@
 class ChaptersPerDateCalculator
 
 
-  def initialize(date_ranges_string)
+  def initialize(begindate:, num_chapters:, num_chapters_per_day: 1)
+
+    @begindate = begindate
+    @num_chapters= num_chapters
+    @num_chapters_per_day = num_chapters_per_day
   end
+
+
+  def schedule
+    {}
+  end
+
+
 
 end
 
