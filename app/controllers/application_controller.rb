3dc28722427455404deb29d235337b80621c95e5
Eric Chen <eric87chen@gmail.com>
2015-07-28 01:48:27 -0400

    Removed unecessary condition

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if @user.challenges.present?
       member_challenges_path
-    else @user.blank?
+    else
       root_path
     end
   end
