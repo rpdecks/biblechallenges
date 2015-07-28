c3854f5f192a72c4674d65ffa45173a45113278f
Eric Chen <eric87chen@gmail.com>
2015-07-28 00:33:21 -0400

    Not showing the oudated challenges

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -3,6 +3,7 @@ class Challenge < ActiveRecord::Base
 
   include PgSearch
   pg_search_scope :search_by_name, against: :name
+  scope :current_challenges, -> {where("enddate >= ?", Date.today)}
 
   include FriendlyId
   # :history option: keeps track of previous slugs
