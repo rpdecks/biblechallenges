a9a0febb8e85c0b905683685c2ce44b6d051cfaa
Philip Bradley <pdbradley@gmail.com>
2015-08-04 14:52:17 -0400

    hotfix to fix broken search if logged in

--- spec/features/visitor_searches_for_a_challenge_spec.rb
+++ spec/features/visitor_searches_for_a_challenge_spec.rb
@@ -12,3 +12,18 @@ feature 'Visitor searches for a challenge' do
   end
 end
 
+feature 'User searches for a challenge' do
+  scenario 'User searches for an existing challenge' do
+    create(:challenge, name: 'Guy')
+    create(:challenge, name: 'Phil')
+
+    user = create(:user)
+    login(user)
+
+    visit challenges_path
+    fill_in "query", with: "Guy"
+    click_button "Search"
+    expect(page).to have_content 'Guy'
+    expect(page).not_to have_content 'Phil'
+  end
+end
