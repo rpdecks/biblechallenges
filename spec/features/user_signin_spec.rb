4d9eb4dc21d9633da5467befb9599688a5cae7d4
guy cao <guy.cao@bfa.org>
2015-08-05 15:50:16 -0400

    test for login from challenge page

--- spec/features/user_signin_spec.rb
+++ spec/features/user_signin_spec.rb
@@ -31,4 +31,23 @@ feature "User Signs In" do
     expect(page).to have_content(challenge.name)
     expect(page).to have_content(challenge2.name)
   end
+
+  context "On challenge page that User not part of" do
+    scenario "Clicks 'Sign me up' and logs in" do
+      skip
+      challenge = create(:challenge, name: "Awesome")
+      visit challenges_path challenge
+      # todo: is there a click_link_or_button
+      # should default be sign me up?
+      click_link "Sign me up"
+      click_link "Login"
+      save_and_open_page
+      within ("#signin-form") do
+        fill_in 'user[email]', with: user.email
+        fill_in 'user[password]', with: "password"
+        click_button "Sign in"
+        expect(current_path).eq challenge_path(challenge)
+      end
+    end
+  end
 end
