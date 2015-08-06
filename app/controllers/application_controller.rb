7cee9152281ebd5432fe8040309685dd2e600f75
guy cao <guy.cao@bfa.org>
2015-08-06 11:40:11 -0700

    save previous url location before logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,32 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
+  before_filter :store_location
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    return unless request.get?
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath
+
+      # to see if the time is recent enough
+      #session[:last_request_time] = Time.now.utc.to_i
+    end
+  end
 
   def render_nothing(status = :ok)
     render json: {}, status: status
   end
 
   def after_sign_in_path_for(resource)
-    if @user.challenges.present?
-      member_challenges_path
+    if session[:previous_url]
+      session[:previous_url]
     else
       root_path
     end
