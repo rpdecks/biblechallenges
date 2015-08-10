e578aa2aeec1035e208752d2d446bbf812e16fea
Eric Chen <eric87chen@gmail.com>
2015-08-10 13:48:22 -0400

    removal of statistics once the corresponding object is deleted by user.

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -28,7 +28,7 @@ class Challenge < ActiveRecord::Base
   has_many :membership_readings, through: :memberships  # needs default order #todo 
   has_many :groups
   has_many :chapters, through: :readings
-  has_many :challenge_statistics
+  has_many :challenge_statistics, dependent: :destroy
 
   belongs_to :owner, class_name: "User", foreign_key: :owner_id
 
