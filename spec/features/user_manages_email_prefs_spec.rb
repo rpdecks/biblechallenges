1ec6b5d2d20b136a70457328690365fc3e0fcd0b
Evan Koch <evankoch@gmail.com>
2015-08-11 15:33:17 -0400

    specs for testing 'manage email prefs'

--- spec/features/user_manages_email_prefs_spec.rb
+++ spec/features/user_manages_email_prefs_spec.rb
@@ -1,55 +1,67 @@
 require 'spec_helper'
 
-feature 'User logs reading via email' do
+feature 'User manages notification preferences via email' do
   include EmailSpec::Helpers
   include EmailSpec::Matchers
 
-  scenario 'User links directly to profile page from the link at the bottom of the daily reading email' do
+  feature 'From daily reading emails' do
+    scenario 'User links directly to profile page from the link at the bottom of the daily reading email' do
+      user = create(:user)
+      login(user)
+      challenge = create(:challenge, :with_readings)
+      create(:membership, user: user, challenge: challenge)
+      #todo needs fixing
+      ReadingMailer.daily_reading_email([challenge.readings.first.id], user.id).deliver_now
 
-    user = create(:user)
-    challenge = create(:challenge, :with_readings)
-    create(:membership, user: user, challenge: challenge)
-    #todo needs fixing
-    ReadingMailer.daily_reading_email([challenge.readings.first.id], user.id).deliver_now
+      open_last_email
+      visit_in_email("Manage your notification preferences.")
+      expect(page).to have_content ("Receive daily readings by email?")
+    end
 
-    open_last_email
-    visit_in_email("Manage your notification preferences.")
-    expect(page).to have_content ("Receive daily readings by email?")
+    scenario 'User gets redirected to sign-in page if not signed in' do
+      user = create(:user)
+      challenge = create(:challenge, :with_readings, chapters_to_read: "Mat 1-2", num_chapters_per_day: 2)
+      create(:membership, user: user, challenge: challenge)
+      reading_ids = challenge.readings.pluck(:id)
+      ReadingMailer.daily_reading_email(reading_ids, user.id).deliver_now
+
+      open_last_email
+      visit_in_email("Manage your notification preferences.")
+      expect(page).to have_content("Log in")
+    end
+
+    scenario 'User updates daily reading email preferences' do
+      user = create(:user)
+      login(user)
+      challenge = create(:challenge, :with_readings)
+      create(:membership, user: user, challenge: challenge)
+      ReadingMailer.daily_reading_email([challenge.readings.first.id], user.id).deliver_now
+
+      open_last_email
+      visit_in_email("Manage your notification preferences.")
+      expect(page).to have_content ("Receive daily readings by email?")
+      uncheck('user_reading_notify')
+      click_button 'Update'
+      user.reload
+      expect(user.reading_notify).to be_falsey
+    end
   end
 
-#  scenario 'User opts out of daily chapter email, and is bypassed on the daily reading email.' do
-#
-#    user = create(:user)
-#    challenge = create(:challenge, :with_readings, chapters_to_read: "Mat 1-2", num_chapters_per_day: 2)
-#    create(:membership, user: user, challenge: challenge)
-#    reading_ids = challenge.readings.pluck(:id)
-#    ReadingMailer.daily_reading_email(reading_ids, user.id).deliver_now
-#
-#    open_last_email
-#    visit_in_email("Wish to stop")
-#    # meta click radio_button (stop sending daily reading email)
-#    click_button 'Update'
-#
-#    expect{
-#    ReadingMailer.daily_reading_email(reading_ids, user.id).To.value
-#    }.not_to eq user.email
-#  end
-#
-#  scenario 'User opts out of comment-notify emails, and is bypassed on the comment-notify mailer.' do
-#
-#    user = create(:user)
-#    challenge = create(:challenge, :with_readings, chapters_to_read: "Mat 1-2", num_chapters_per_day: 2)
-#    create(:membership, user: user, challenge: challenge)
-#    reading_ids = challenge.readings.pluck(:id)
-#    ReadingMailer.daily_reading_email(reading_ids, user.id).deliver_now
-#
-#    open_last_email
-#    visit_in_email("Wish to stop")
-#    # meta click radio_button (stop sending daily reading email)
-#    click_button 'Update'
-#
-#    expect{
-#    ReadingMailer.daily_reading_email(reading_ids, user.id).To.value
-#    }.not_to eq user.email
-#  end
+  feature 'From comment emails' do
+    scenario 'User opts out of comment-notify emails, and is bypassed on the comment-notify mailer.' do
+      user = create(:user)
+      user2 = create(:user)
+      login(user)
+      create(:challenge, :with_readings)
+      original_comment = create(:group_comment, user: user2)
+      new_comment = create(:group_comment, commentable: original_comment, user: user, content: "cool dude")
+      CommentMailer.new_comment_notification(new_comment).deliver_now
+      open_last_email
+      visit_in_email("here")
+      uncheck('user_comment_notify')
+      click_button 'Update'
+      user.reload
+      expect(user.comment_notify).to be_falsey
+    end
+  end
 end
