57784d038ab82dc4ae636b3435b4fc1608364252
Philip Bradley <pdbradley@gmail.com>
2015-08-13 16:00:21 -0400

    migration to populate available_dates in challneges

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813191244) do
+ActiveRecord::Schema.define(version: 20150813194635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
