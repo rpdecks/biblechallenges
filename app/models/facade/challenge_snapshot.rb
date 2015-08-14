2fb1b61fa62181a25561797f308ad10a4c4d70be
Philip Bradley <pdbradley@gmail.com>
2015-08-14 10:58:54 -0400

    more snapshot details

--- app/models/facade/challenge_snapshot.rb
+++ app/models/facade/challenge_snapshot.rb
@@ -2,6 +2,7 @@ class ChallengeSnapshot
 
   def initialize(challenge)
     @challenge = challenge
+    @top_half_limit = @challenge.memberships.size / 2
   end
 
 
@@ -10,6 +11,15 @@ class ChallengeSnapshot
       order('group_statistics.value desc')
   end
 
+  def groups_by_on_schedule_percentage
+    @challenge.groups.joins(:group_statistic_on_schedule_percentage).
+      order('group_statistics.value desc')
+  end
+
+  def memberships_by_percentage_challenge_completed
+    @challenge.memberships.joins(:membership_statistic_progress_percentage).
+      order('membership_statistics.value desc').limit(@top_half_limit)
+  end
 
 
 
