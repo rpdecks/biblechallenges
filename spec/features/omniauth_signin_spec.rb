efb87d239ad1c7cedaeb9b2efab5fdf6431ea170
guy cao <guy.cao@bfa.org>
2015-08-17 12:11:14 -0700

    test for sign in

--- spec/features/omniauth_signin_spec.rb
+++ spec/features/omniauth_signin_spec.rb
@@ -64,4 +64,26 @@ feature "Omniauth Sign In / Sign Up" do
     click_button "Continue"
     expect(page).to have_content("Password confirmation doesn't match Password")
   end
+
+  scenario "New User SIGNS UP with devise then signs in with facebook" do
+    name = Faker::Name.name
+    email = "barry.allen@flash.com"
+    password = Faker::Internet.password
+
+    visit root_path
+    click_link "Login"
+    click_link "Sign up"
+    fill_in 'user[name]', with: name
+    fill_in 'user[email]', with: email
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: password
+    click_button "Sign up"
+    expect(page).to have_content("You have signed up successfully")
+    click_link "Logout"
+
+    mock_auth_hash("facebook", email)
+    click_link "Sign in with Facebook"
+    expect(page).to have_content("Signed in successfully")
+  end
+
 end
