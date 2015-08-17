1854ca4e4cbf08bc2fe7cbea9bc2d00be944838a
Eric Chen <eric87chen@gmail.com>
2015-08-17 14:49:22 -0400

    Removed unnecessary code

--- spec/factories/identities.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :identity do
-    uid "MyString"
-    provider "MyString"
-    user ""
-  end
-end
