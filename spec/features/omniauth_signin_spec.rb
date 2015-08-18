67baff8694f9a324c923c8a1111e8e1088c4e642
Eric Chen <eric87chen@gmail.com>
2015-08-18 15:47:53 -0400

    Added more omniauth tests

--- spec/features/omniauth_signin_spec.rb
+++ spec/features/omniauth_signin_spec.rb
@@ -32,8 +32,25 @@ feature "Omniauth Sign In / Sign Up" do
     expect(page).to have_content("Signed in successfully")
   end
 
+  scenario "New User SIGNS UP with google, logs out, logs back in with facebook" do
+    email = "peter.parker@spider.man"
+    mock_auth_hash("google_oauth2", email)
+    password = Faker::Internet.password
+    visit root_path
+    click_link "Sign in with Google"
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: password
+    click_button "Continue"
+    expect(page).to have_content("You have signed up successfully")
+
+    click_link "Logout"
+
+    mock_auth_hash("facebook", email)
+    click_link "Sign in with Facebook"
+    expect(page).to have_content("Successfully authenticated")
+  end
+
   scenario "New User SIGNS UP with google, logs out, logs back in" do
-    pending "removed google sign up temporarily"
     email = "peter.parker@spider.man"
     mock_auth_hash("google_oauth2", email)
     password = Faker::Internet.password
@@ -83,7 +100,28 @@ feature "Omniauth Sign In / Sign Up" do
 
     mock_auth_hash("facebook", email)
     click_link "Sign in with Facebook"
-    expect(page).to have_content("Signed in successfully")
+    expect(page).to have_content("Successfully authenticated")
+  end
+
+  scenario "New User SIGNS UP with devise then signs in with google" do
+    name = Faker::Name.name
+    email = "barry.allen@flash.com"
+    password = Faker::Internet.password
+
+    visit root_path
+    click_link "Login"
+    click_link "Sign up"
+    fill_in 'user[name]', with: name
+    fill_in 'user[email]', with: email
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: password
+    click_button "Sign up"
+    expect(page).to have_content("You have signed up successfully")
+    click_link "Logout"
+
+    mock_auth_hash("google_oauth2", email)
+    click_link "Sign in with Google"
+    expect(page).to have_content("Successfully authenticated")
   end
 
 end
