376fe1684b0e5c5009d7f06b2bf11c5635353717
Evan Koch <evankoch@gmail.com>
2015-08-19 16:35:08 -0400

    merged-in master

--- app/workers/daily_email_worker.rb
+++ app/workers/daily_email_worker.rb
@@ -1,7 +1,7 @@
 class DailyEmailWorker
   include Sidekiq::Worker
   #if there is a problem with the email we don't want the worker retrying the job
-  sidekiq_options retry: 3
+  sidekiq_options retry: false
 
   def perform(readings, member)
     ReadingMailer.daily_reading_email(readings, member).deliver_now
--- spec/features/user_visits_challenges_spec.rb
+++ spec/features/user_visits_challenges_spec.rb
@@ -13,9 +13,18 @@ feature 'User visits challenges' do
       challenge = create(:challenge_with_readings, chapters_to_read: "Matt 1-2", begindate: ch1_start_date)
     Timecop.return
     ch2_start_date = Date.today
-      challenge2 = create(:challenge_with_readings, chapters_to_read: "Matt 1-2", begindate: ch2_start_date)
+    challenge2 = create(:challenge, :with_5_memberships, chapters_to_read: "Matt 1-2", begindate: ch2_start_date)
     visit root_path
     expect(page).to_not have_content(challenge.name)
     expect(page).to have_content(challenge2.name)
+  end
+
+  scenario 'User cannot see challenge details that he is not a part of' do
+    challenge = create(:challenge)
+    user1 = challenge.owner
+    challenge.join_new_member(user1)
+    visit member_challenge_path(challenge)
+    expect(page).to_not have_content("Today's Reading")
+    expect(page).to have_content("Join Challenge")
   end
 end
