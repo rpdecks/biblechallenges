456c1ae1580c4f0da191558e43f8635efc35a7f9
Evan Koch <evankoch@gmail.com>
2015-08-20 15:38:34 -0400

    Added data for front-page leaderboard: decorator to display user names, scopes for displayed stats

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,4 +1,21 @@
 class UserDecorator < Draper::Decorator
-  include PrettyDate
+
   delegate_all
+
+  def initialed_name
+    parts_of_name = name.split
+    result = ""
+    parts_of_name.each do |name_part|
+      # we want the entire last part of the name field
+      if name_part == parts_of_name.last
+        result += name_part
+      else
+        result += "#{name_part[0]}. "
+      end
+    end
+    result
+  end
+
+
+
 end
