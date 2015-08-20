0fe000b5dc10dd4c24a815b1ebf936005bd26673
Philip Bradley <pdbradley@gmail.com>
2015-08-20 17:12:51 -0400

    small tweaks to layout

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -3,14 +3,16 @@ class UserDecorator < Draper::Decorator
   delegate_all
 
   def initialed_name
-    parts_of_name = name.split
     result = ""
-    parts_of_name.each do |name_part|
-      # we want the entire last part of the name field
-      if name_part == parts_of_name.last
-        result += name_part
-      else
-        result += "#{name_part[0]}. "
+    if name
+      parts_of_name = name.split
+      parts_of_name.each do |name_part|
+        # we want the entire last part of the name field
+        if name_part == parts_of_name.last
+          result += name_part
+        else
+          result += "#{name_part[0]}. "
+        end
       end
     end
     result
