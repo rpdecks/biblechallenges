1ec2568d08d8bec25dc81152a1cf6d5cae9da076
Philip Bradley <pdbradley@gmail.com>
2015-08-21 16:35:52 -0400

    fixes for branko

--- app/models/facade/front_page_leaderboard.rb
+++ app/models/facade/front_page_leaderboard.rb
@@ -1,5 +1,8 @@
 class FrontPageLeaderboard
 
+  include ActionView::Helpers::DateHelper
+  include ApplicationHelper
+
   DEFAULT_LIMIT = 10
 
   def users_by_longest_all_time_streak
@@ -16,5 +19,27 @@ class FrontPageLeaderboard
     User.joins(:user_statistic_chapters_read_all_time).
       order('user_statistics.value desc').limit(DEFAULT_LIMIT).decorate
   end
+
+  def users_by_most_recent_reading
+    # gonna make an easy access collection here :)
+    most_recent_membership_readings.map do |mr|
+      user = OpenStruct.new
+      user.user = mr.user
+      user.initaled_name = mr.user.decorate.initialed_name
+      user.chapter_number = mr.chapter.chapter_number
+      user.book_name = mr.chapter.book_name
+      user.time_ago = time_ago_in_words(mr.created_at) + " ago"
+      user
+    end
+  end
+
+  private
+
+  def most_recent_membership_readings
+    MembershipReading.unscoped.
+      joins(:user, :reading, :chapter).
+      order('membership_readings.created_at desc').limit(DEFAULT_LIMIT)
+  end
+
 
 end
