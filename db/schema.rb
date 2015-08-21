5f55bad0b2e7cdab52f4ab472ca201e4fd902656
Eric Chen <eric87chen@gmail.com>
2015-08-21 11:42:14 -0400

    Added status column to challenge table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813194635) do
+ActiveRecord::Schema.define(version: 20150821152051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,14 +58,15 @@ ActiveRecord::Schema.define(version: 20150813194635) do
     t.string   "dates_to_skip"
     t.integer  "memberships_count"
     t.integer  "readings_count"
-    t.integer  "book_chapters",        default: [],              array: true
+    t.integer  "book_chapters",        default: [],                  array: true
     t.text     "date_ranges_to_skip"
-    t.integer  "days_of_week_to_skip", default: [],              array: true
+    t.integer  "days_of_week_to_skip", default: [],                  array: true
     t.string   "slug"
     t.integer  "num_chapters_per_day", default: 1
-    t.hstore   "chapters_per_date",    default: {}, null: false
-    t.json     "schedule",             default: {}, null: false
-    t.date     "available_dates",      default: [],              array: true
+    t.hstore   "chapters_per_date",    default: {},     null: false
+    t.json     "schedule",             default: {},     null: false
+    t.date     "available_dates",      default: [],                  array: true
+    t.string   "status",               default: "open"
   end
 
   add_index "challenges", ["owner_id"], name: "index_challenges_on_owner_id", using: :btree
