2342698902fc9726ea9722bf344d8c9c043ebadf
Branko Tomić <klomontes@gmail.com>
2015-08-23 23:39:07 +0200

    Added bootstrap switch markup and JS

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -2,4 +2,6 @@ $( document ).ready(function() {
   $(".alert-info").fadeTo(2000, 500).slideUp(500, function(){
     $(".alert-info").alert('close');
   });
+
+  $("[name='challenge-activity']").bootstrapSwitch();
 });
