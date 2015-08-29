56c0c4cf7a84df798f750587342846fc35ab1c91
guy cao <guy.cao@bfa.org>
2015-08-28 17:45:21 -0700

    record readings and schedule time

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150828174628) do
+ActiveRecord::Schema.define(version: 20150829003328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,6 +112,8 @@ ActiveRecord::Schema.define(version: 20150828174628) do
     t.string   "email"
     t.string   "time_zone"
     t.integer  "preferred_reading_hour"
+    t.datetime "schedule_time"
+    t.integer  "readings",                            array: true
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
--- spec/models/email_log_spec.rb
+++ spec/models/email_log_spec.rb
@@ -10,13 +10,15 @@ describe EmailLog, type: :model do
       DailyEmailScheduler.set_daily_email_jobs
     }.to change(EmailLog, :count).by(2)
 
-    last_log = EmailLog.first
-    expect(last_log.challenge_id).to eq challenge.id
-    expect(last_log.user_id).to eq user.id
-    expect(last_log.email).to eq user.email
-    expect(last_log.time_zone).to eq user.time_zone
-    expect(last_log.preferred_reading_hour).
+    log = EmailLog.first
+    expect(log.challenge_id).to eq challenge.id
+    expect(log.user_id).to eq user.id
+    expect(log.email).to eq user.email
+    expect(log.time_zone).to eq user.time_zone
+    expect(log.preferred_reading_hour).
       to eq user.preferred_reading_hour
+
+    expect(log.schedule_time).to_not be_nil
   end
 
   it "logs email delivering" do
@@ -31,5 +33,9 @@ describe EmailLog, type: :model do
       DailyEmailWorker.perform_async(readings_ids, challenge.owner.id)
       DailyEmailWorker.drain
     }.to change(EmailLog, :count).by(2)
+
+    log = EmailLog.first
+    expect(log.schedule_time).to be_nil
+    expect(log.readings).to eq readings_ids
   end
 end
