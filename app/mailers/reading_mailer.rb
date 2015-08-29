e1268400798af4b984ff3c1a301b4c1f2a4ca08d
guy cao <guy.cao@bfa.org>
2015-08-28 17:29:35 -0700

    log email delivery

--- app/mailers/reading_mailer.rb
+++ app/mailers/reading_mailer.rb
@@ -12,6 +12,14 @@ class ReadingMailer < ActionMailer::Base
     @membership = Membership.where(user: @user, challenge: @challenge).first
 
     unless @membership.nil?
+          EmailLog.create(event: "Deliver",
+                          challenge_id: @challenge.id,
+                          user_id: @user.id,
+                          email: @user.email,
+                          time_zone: @user.time_zone,
+                          preferred_reading_hour: @user.preferred_reading_hour)
+          #include readings
+
       mail( to: @user.email,
            subject: "Bible Challenge reading for #{@challenge.name}",
       from: "#{@challenge.name.capitalize} <no-reply@biblechallenges.com>")
