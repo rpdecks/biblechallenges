f8d6276cb1e46164e2edae35efa9766ff6ec7fd0
guy cao <guy.cao@bfa.org>
2015-08-31 11:36:07 -0700

    refactor challenge tests

--- app/mailers/challenge_mailer.rb
+++ app/mailers/challenge_mailer.rb
@@ -9,7 +9,8 @@ class ChallengeMailer < ActionMailer::Base
   def creation_email(challenge_id)
     @challenge = Challenge.friendly.find(challenge_id)
     @owner = @challenge.owner
-    mail( to: @owner.email, subject: "Bible Challenge #{@challenge.name} created!")
+    mail( to: @owner.email,
+         subject: "Bible Challenge #{@challenge.name} created!")
   end
 
 end
