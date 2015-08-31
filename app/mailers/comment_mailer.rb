87de6fb9da14dbbbe690aa178f004674850dd07d
guy cao <guy.cao@bfa.org>
2015-08-31 12:23:00 -0700

    make default from consistent. remove remainin from fields

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,12 +1,14 @@
 class CommentMailer < ActionMailer::Base
-  default from: "noreply@biblechallenge.com"
+  default from: "Bible Challenges <no-reply@biblechallenges.com>"
 
   def new_comment_notification(comment)
     @recipient = comment.commentable.user
     @original_comment = comment.commentable
     @newcomment = comment
 
-    mail( to: @recipient.email,
-      subject: "#{comment.user.name} has responded to your comment")
+    mail(
+      to: @recipient.email,
+      subject: "#{comment.user.name} has responded to your comment"
+    )
   end
 end
