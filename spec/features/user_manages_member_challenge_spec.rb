d4f24b337585fab992196adda7627ee4c265355f
guy cao <guy.cao@bfa.org>
2015-09-08 15:07:51 -0700

    test todays reading account for timezone

--- spec/features/user_manages_member_challenge_spec.rb
+++ spec/features/user_manages_member_challenge_spec.rb
@@ -7,22 +7,43 @@ feature 'User manages member/challenge' do
     login(user)
   end
 
-  scenario 'User is able to see Todays reading', :js => true do
-    #pending "TODO: Need to test content within javascript generated view"
-    challenge = create(:challenge_with_readings, chapters_to_read: "Matthew 1")
+  scenario "User is able to see Todays reading", :js => true do
+    challenge = create(:challenge_with_readings, chapters_to_read: "Matthew 1-3")
     ChallengeCompletion.new(challenge)
     create(:membership, challenge: challenge, user: user)
+    #pending "TODO: Need to test content within javascript generated view"
     visit member_challenge_path(challenge)
     #wait_for_ajax
     expect(page).to have_content("David")
   end
 
-  scenario 'User is able to log todays reading' do
-    challenge = create(:challenge_with_readings, chapters_to_read: "Matthew 1")
+  scenario "User is able to log todays reading" do
+    challenge = create(:challenge_with_readings, chapters_to_read: "Matthew 1-3")
     ChallengeCompletion.new(challenge)
     create(:membership, challenge: challenge, user: user)
     visit member_challenge_path(challenge)
     click_link "Log #{challenge.readings.first.book_and_chapter}"
     expect(MembershipReading.count).to eq 1
   end
+
+  scenario "Today's reading accounts for Timezone" do
+    user.time_zone = "Paris"
+
+    creation_time = Time.local(2015, 7, 4, 10, 0, 0) # time challenge was created
+    Timecop.travel(creation_time)
+    challenge = create(:challenge_with_readings, chapters_to_read: "Matthew 1-3")
+    ChallengeCompletion.new(challenge)
+    create(:membership, challenge: challenge, user: user)
+
+    read_time = Time.local(2015, 7, 5, 23, 0, 0) # a day later, at night
+    Timecop.travel(read_time)
+    visit member_challenge_path(challenge)
+
+    expect(page).to_not have_content("Matthew 2")
+    expect(page).to have_content("Matthew 3")
+
+    Timecop.return
+  end
+
+  scenario "Today's reading accounts for multiple readings per day"
 end
