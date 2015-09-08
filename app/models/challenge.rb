70dda71478517ba55de417a50ba5f0a2e6799300
guy cao <guy.cao@bfa.org>
2015-09-08 16:48:04 -0700

    use users timezone when displaying readings

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -136,9 +136,10 @@ class Challenge < ActiveRecord::Base
     readings.find_by_read_on(Date.today)
   end
 
-  def todays_readings
-    # todo: take time zone into consideration
-    readings.where(read_on: Time.zone.now.to_date)
+  def todays_readings(user)
+    userdate = Time.now.in_time_zone(user.time_zone).to_date
+    # todo: what if time_zone is null?
+    readings.where(read_on: userdate)
   end
 
   def all_users_emails_except_challenge_owner
