f3a4f3ab1a46ec08215766c89780fbf070e4309a
Evan Koch <evankoch@gmail.com>
2015-09-18 15:09:37 -0400

    added 'log-reading' button on challenge readings show page

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -13,7 +13,7 @@ class ReadingsController < ApplicationController
     if @reading.challenge.members.include? current_user
       @comment = Comment.new
       @comments = @reading.comments.order("created_at desc")
-      @last_read_by = @reading.last_read_by  
+      @last_read_by = @reading.last_read_by
       @membership = Membership.find_by_user_id_and_challenge_id(current_user.id, @reading.challenge.id)
       @verses = @reading.chapter.verses.by_version(@membership.bible_version)
     else
