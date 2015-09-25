620a222849a94d1bde051e16c2de9a0918354264
Evan Koch <evankoch@gmail.com>
2015-09-25 16:14:20 -0400

    modified next_chapter method on reading model to account for multiple chapters per read_on day

--- spec/features/user_views_readings_spec.rb
+++ spec/features/user_views_readings_spec.rb
@@ -3,13 +3,15 @@ require 'spec_helper'
 feature 'User manages readings' do
   background do
     @user = create(:user)
-    @challenge = create(:challenge_with_readings, chapters_to_read: "Matthew 1-2")
+    @challenge = create(:challenge, name: "Testy Challenge", chapters_to_read: "Matthew 1-9", owner: @user, begindate: Date.today, enddate: Date.tomorrow)
     create(:membership, challenge: @challenge, user: @user)
+    @challenge.generate_readings
     login(@user)
   end
 
   scenario "sees references to the reading pages" do
     visit challenge_path(@challenge)
+    save_and_open_page
     expect(page).to have_link "Matthew 1"
   end
 
@@ -17,5 +19,13 @@ feature 'User manages readings' do
     visit challenge_path(@challenge)
     click_link "Matthew 1"
     expect(current_path).to eq reading_path(@challenge.readings.first)
+  end
+
+  scenario "from chapters reading page sees and links to 'next chapter'" do
+    visit challenge_path(@challenge)
+    click_link "Matthew 2"
+    expect(page).to have_link "Matthew 3"
+    click_link "Matthew 3"
+    expect(current_path).to eq reading_path(@challenge.readings.third)
   end
 end
