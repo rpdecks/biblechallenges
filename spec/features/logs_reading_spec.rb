96cb6248f5e447072f5624ab8db43e2812a66399
guy cao <guy.cao@bfa.org>
2015-09-28 11:27:58 -0700

    wip update test

--- spec/features/logs_reading_spec.rb
+++ spec/features/logs_reading_spec.rb
@@ -18,27 +18,42 @@ feature "User logs reading" do
     }.to change(user.membership_readings, :count).by(1)
   end
 
-  scenario "correctly calculate statistics using user's timezone" do
-    skip "look at controller tests first"
-    challenge = create(:challenge_with_readings, chapters_to_read: "Matthew 1-2")
+  scenario "correctly calculate user statistics using user's timezone" do
+    user.time_zone = "Paris"
+
+    #create a challenge
+    Time.zone = "Eastern Time (US & Canada)"
+    creation_time = Time.local(2015, 7, 4, 23, 0, 0) # time challenge was created
+    Timecop.travel(creation_time)
+    challenge = create(:challenge_with_readings,
+                       chapters_to_read: "Matthew 1-2",
+                       owner: user)
     ChallengeCompletion.new(challenge)
-    membership = create(:membership, user: user, challenge: challenge)
-    MembershipStatisticAttacher.attach_statistics(membership)
+    create(:membership, user: user, challenge: challenge)
+    user.associate_statistics
 
-    visit member_challenge_path(challenge)
-    click_link_or_button "Log Matthew 1"
-    #UpdateStatsWorker.drain
+    Sidekiq::Testing.inline! do
+      visit member_challenge_path(challenge)
+      click_link_or_button "Log Matthew 1"
 
-    Timecop.travel(1.day)
-    visit member_challenge_path(challenge)
-    click_link_or_button "Log Matthew 2"
+      expect(user.user_statistic_days_read_in_a_row_all_time.value).to eq 1
+      expect(user.user_statistic_days_read_in_a_row_current.value).to eq 1
 
-    expect(user.user_statistic_chapters_read_all_time.value).to eq 2
-    expect(user.user_statistic_days_read_in_a_row_all_time.value).to eq 2
-    expect(user.user_statistic_days_read_in_a_row_current.value).to eq 2
+      second_day_read_time = Time.local(2015, 7, 5, 23, 0, 0) # a day later, at night
+      Timecop.travel(second_day_read_time)
+      binding.pry
+      visit member_challenge_path(challenge)
+      click_link_or_button "Log Matthew 2"
+
+      user.reload
+      expect(user.user_statistic_days_read_in_a_row_all_time.value).to eq 2
+      expect(user.user_statistic_days_read_in_a_row_current.value).to eq 2
+    end
 
     Timecop.return
   end
+
+  scenario "log reading via email correctly"
 
   scenario "via challenge page readings table checkbox"
 end
