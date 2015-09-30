a52983713a15d85e0c1bd378900232ca3bfd0734
guy cao <guy.cao@bfa.org>
2015-09-30 14:07:27 -0700

    additional tests for stats

--- app/models/statistics/membership_statistic_on_schedule_percentage.rb
+++ app/models/statistics/membership_statistic_on_schedule_percentage.rb
@@ -9,7 +9,8 @@ class MembershipStatisticOnSchedulePercentage < MembershipStatistic
   end
 
   def calculate
-    readings_to_date = membership.readings.to_date(Time.current.utc.yesterday.to_date)
+    readings_to_date = membership.readings.to_date(Date.today - 1.day)
+
     on_schedule_membership_readings = membership.membership_readings.on_schedule
     on_time_readings = 0
 
@@ -19,7 +20,7 @@ class MembershipStatisticOnSchedulePercentage < MembershipStatistic
       end
     end
 
-    readings_to_date.size.zero? ? 0 : (on_time_readings * 100) / readings_to_date.size 
+    readings_to_date.size.zero? ? 0 : (on_time_readings * 100) / readings_to_date.size
   end
 
   def update
--- spec/features/logs_reading_spec.rb
+++ spec/features/logs_reading_spec.rb
@@ -18,7 +18,69 @@ feature "User logs reading" do
     }.to change(user.membership_readings, :count).by(1)
   end
 
-  scenario "correctly calculate user statistics using user's timezone" do
+  scenario "Calculate membership on schedule percentage" do
+    user.time_zone = "Pacific Time (US & Canada)"
+
+    # create challenge
+    challenge_date = Date.new(2015, 7, 4)
+    challenge = create(:challenge_with_readings,
+                       chapters_to_read: "Matthew 1-3",
+                       owner: user, begindate: challenge_date)
+    membership = create(:membership, user: user, challenge: challenge)
+    membership.associate_statistics
+
+    Sidekiq::Testing.inline! do
+      Time.zone = user.time_zone
+      first_day_read_time = Time.zone.local(2015, 7, 4, 23, 0, 0) # for user at PST
+      Timecop.travel(first_day_read_time)
+
+      Time.zone = "UTC" # heroku timezone
+      visit member_challenge_path(challenge)
+      click_link_or_button "Log Matthew 1"
+
+      Timecop.travel(1.day)
+      visit member_challenge_path(challenge)
+      click_link_or_button "Log Matthew 2"
+
+      expect(membership.membership_statistics.find_by_type(
+        "MembershipStatisticOnSchedulePercentage").value).to eq 100
+
+      Timecop.return
+    end
+  end
+
+  scenario "Calculate user current streak based on timezone" do
+    user.time_zone = "Paris"
+
+    # create challenge
+    challenge_date = Date.new(2015, 7, 4)
+    challenge = create(:challenge_with_readings,
+                       chapters_to_read: "Matthew 1-3",
+                       owner: user, begindate: challenge_date)
+    create(:membership, user: user, challenge: challenge)
+    user.associate_statistics
+
+    Sidekiq::Testing.inline! do
+      Time.zone = user.time_zone
+      first_day_read_time = Time.zone.local(2015, 7, 4, 23, 0, 0) # for user at PST
+      Timecop.travel(first_day_read_time)
+
+      Time.zone = "UTC" # heroku timezone
+      visit member_challenge_path(challenge)
+      click_link_or_button "Log Matthew 1"
+
+      Timecop.travel(1.day)
+      visit member_challenge_path(challenge)
+      click_link_or_button "Log Matthew 2"
+
+      expect(user.user_statistic_days_read_in_a_row_all_time.value).to eq 2
+      expect(user.user_statistic_days_read_in_a_row_current.value).to eq 2
+
+      Timecop.return
+    end
+  end
+
+  scenario "correctly calculate personal statistics using user's timezone" do
     user.time_zone = "Paris"
 
     # create a challenge
@@ -36,7 +98,7 @@ feature "User logs reading" do
     # log readings
     Sidekiq::Testing.inline! do
       visit member_challenge_path(challenge)
-      click_link_or_button "Log Matthew 2"
+      click_link_or_button "Log Matthew 1"
 
       expect(user.user_statistic_days_read_in_a_row_all_time.value).to eq 1
       expect(user.user_statistic_days_read_in_a_row_current.value).to eq 1
@@ -44,7 +106,7 @@ feature "User logs reading" do
       second_day_read_time = Time.local(2015, 7, 5, 23, 0, 0) # a day later, at night
       Timecop.travel(second_day_read_time)
       visit member_challenge_path(challenge)
-      click_link_or_button "Log Matthew 3"
+      click_link_or_button "Log Matthew 2"
 
       user.reload
       expect(user.user_statistic_days_read_in_a_row_all_time.value).to eq 2
@@ -54,7 +116,7 @@ feature "User logs reading" do
     Timecop.return
   end
 
-  scenario "Same challenge, 2 users in different Time zones reads through checking stats" do
+  scenario "Challenge stats for 2 users in different Timezones" do
     user.time_zone = "Hawaii"
     user2 = create(:user)
     user2.time_zone = "Greenland"
@@ -63,7 +125,7 @@ feature "User logs reading" do
     Time.zone = user.time_zone
     creation_time = Time.zone.local(2015, 7, 4, 23, 0, 0) # time challenge was created
     Timecop.travel(creation_time)
-    c = create(:challenge, chapters_to_read: "Mat 1-2", begindate: "2015-07-04")
+    c = create(:challenge, chapters_to_read: "Matt 1-2", begindate: "2015-07-04")
     c.generate_readings
     c.associate_statistics
 
