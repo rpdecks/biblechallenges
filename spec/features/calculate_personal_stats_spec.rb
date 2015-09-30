cb555a9f530a17383eff925a3aac4aa61dec6218
guy cao <guy.cao@bfa.org>
2015-09-30 15:24:53 -0700

    fix user stat current streak uniq

--- spec/features/calculate_personal_stats_spec.rb
+++ spec/features/calculate_personal_stats_spec.rb
@@ -7,39 +7,24 @@ feature "calculates personal stats" do
     login(user)
   }
 
-  scenario "challenge in eastern time. user in paris" do
-    user.time_zone = "Paris"
+  context "multiple chapters logged per day" do
+    scenario "calculate current streaks correctly" do
+      challenge = create(:challenge_with_readings,
+                         owner: user)
+      m = create(:membership, user: user, challenge: challenge)
+      user.associate_statistics
+      create_list(:membership_reading, 2, membership: m) # day 1 with two readings
+      Timecop.travel(1.day)
+      create_list(:membership_reading, 2, membership: m) # day 2 with two readings
+      Timecop.travel(1.day)
+      create_list(:membership_reading, 2, membership: m) # day 3 with two readings
 
-    # create challenge
-    Time.zone = "Eastern Time (US & Canada)"
-    challenge_creation_time = Time.zone.local(2015, 7, 4, 23, 0, 0) # time challenge was created
-    Timecop.travel(challenge_creation_time)
-    challenge = create(:challenge_with_readings,
-                       chapters_to_read: "Matthew 1-3",
-                       owner: user)
-    binding.pry
-    create(:membership, user: user, challenge: challenge)
-    user.associate_statistics
+      user.update_stats
+      user.reload
+      expect(user.user_statistic_days_read_in_a_row_current.value).to eq 3
 
-    Sidekiq::Testing.inline! do
-      visit member_challenge_path(challenge)
-      click_link_or_button "Log Matthew 1"
-      expect(user.user_statistic_days_read_in_a_row_current.value).to eq 1
-
-      #Timecop.travel(1.day)
-      #visit member_challenge_path(challenge)
-      #click_link_or_button "Log Matthew 2"
-      #user.reload
-      #expect(user.user_statistic_days_read_in_a_row_current.value).to eq 2
-
-      #Timecop.travel(1.day)
-      #visit member_challenge_path(challenge)
-      #click_link_or_button "Log Matthew 3"
-      #user.reload
-      #expect(user.user_statistic_days_read_in_a_row_current.value).to eq 3
+      Timecop.return
     end
-
-    Timecop.return
   end
 
   scenario "challenge in eastern time. user in paris" do
