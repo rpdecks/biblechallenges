02764a9308c0b397e96dcf37201cedc54fa0f96a
guy cao <guy.cao@bfa.org>
2015-10-12 11:43:27 -0700

    refactor membership generator and test

--- spec/models/service/membership_generator_spec.rb
+++ spec/models/service/membership_generator_spec.rb
@@ -3,11 +3,9 @@ require 'spec_helper'
 describe MembershipGenerator do
   describe "#generate_membership" do
     it "creates membership for one or more users" do
-      user1 = create(:user)
-      user2 = create(:user)
-      user3 = create(:user)
+      users = create_list(:user, 3)
       challenge = create(:challenge)
-      MembershipGenerator.new(challenge, user1, user2, user3).generate
+      MembershipGenerator.new(challenge, users).generate
       expect(Membership.count).to eq 3
     end
   end
