aa3d25cfedbcf24451bdba620537028324d015ac
Eric Chen <eric87chen@gmail.com>
2015-10-12 15:33:57 -0400

    Refactored the extracting of members from previous challenge

--- spec/models/service/membership_generator_spec.rb
+++ spec/models/service/membership_generator_spec.rb
@@ -2,11 +2,18 @@ require 'spec_helper'
 
 describe MembershipGenerator do
   describe "#generate_membership" do
-    it "creates membership for one or more users" do
+    it "creates membership for multiple users" do
       users = create_list(:user, 3)
       challenge = create(:challenge)
       MembershipGenerator.new(challenge, users).generate
       expect(Membership.count).to eq 3
+    end
+
+    it "creates membership for one user" do
+      user = create(:user)
+      challenge = create(:challenge)
+      MembershipGenerator.new(challenge, user).generate
+      expect(Membership.count).to eq 1
     end
   end
 end
