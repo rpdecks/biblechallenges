9a13076618b934f2f4df4f897139ff9ca15ffe3f
Eric Chen <eric87chen@gmail.com>
2015-10-14 11:11:39 -0400

    Fixed broken tests

--- spec/models/service/imports_members_from_previous_challenge_spec.rb
+++ spec/models/service/imports_members_from_previous_challenge_spec.rb
@@ -4,10 +4,12 @@ describe ImportsMembersFromPreviousChallenge do
   describe "#import" do
     it "imports members from previous challenge for creating a new challenge" do
       users = create_list(:user, 3)
-      challenge = create(:challenge)
-      challenge.join_new_member(users)
-      imfpc = ImportsMembersFromPreviousChallenge.new(challenge)
-      expect(imfpc.import).to eq users
+      old_challenge = create(:challenge, owner_id: users.first.id)
+      old_challenge.join_new_member(users)
+      new_challenge = create(:challenge, owner_id: users.first.id)
+      ImportsMembersFromPreviousChallenge.new(old_challenge.id, new_challenge).import
+      new_challenge.reload
+      expect(old_challenge.members).to eq new_challenge.members
     end
   end
 end
