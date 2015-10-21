cf80c2dad6803f5bd29ba2d980a2e8263b327045
Eric Chen <eric87chen@gmail.com>
2015-10-21 11:02:12 -0400

    Updated membership completion to handle group name and id

--- app/models/service/membership_reading_completion.rb
+++ app/models/service/membership_reading_completion.rb
@@ -1,8 +1,9 @@
 class MembershipReadingCompletion
-  def initialize(user, challenge, membership_reading)
+  def initialize(user, membership, membership_reading)
     @user = user
-    @challenge = challenge
+    @challenge = membership.challenge
     @membership_reading = membership_reading
+    @group = membership.group if membership.group
   end
 
   def attach_attributes
@@ -11,6 +12,8 @@ class MembershipReadingCompletion
     @membership_reading.challenge_name= @challenge.name
     @membership_reading.chapter_id = @membership_reading.chapter.id
     @membership_reading.chapter_name = @membership_reading.chapter.book_and_chapter
+    @membership_reading.group_name = @group.name if @group
+    @membership_reading.group_id = @group.id if @group
     @membership_reading.save
   end
 end
