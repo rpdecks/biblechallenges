60fc30981499237a19e845e65f47f39691802081
Eric Chen <eric87chen@gmail.com>
2015-10-23 17:08:51 -0400

    Removed tests, hide the unsubscribe link from challenge owners

--- spec/features/user_unsubscribes_via_email_spec.rb
+++ spec/features/user_unsubscribes_via_email_spec.rb
@@ -19,22 +19,6 @@ feature 'User unsubscribes via email' do
     }.to change(Membership, :count).by(-1)
 
   end
-
-  scenario 'Owner unsubscribes from a challenge and in doing so, deletes the challenge altogether' do
-
-    user = create(:user)
-    challenge = create(:challenge, :with_readings, owner_id: user.id)
-    create(:membership, user: user, challenge: challenge)
-    ReadingMailer.daily_reading_email([challenge.readings.first.id], user.id).deliver_now
-
-    open_last_email
-    path = parse_email_for_link(current_email, "Here")# the link is Click *here* to unsubsubscribe
-    visit(path)
-    click_button("Unsubscribe Me")
-    expect(Challenge.all.size).to eq 0
-    expect(Membership.all.size).to eq 0
-
-  end
 end
 
 
