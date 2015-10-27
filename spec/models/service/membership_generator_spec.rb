f82131c7d35aed501fabcdd45ddc18d8259783bf
Eric Chen <eric87chen@gmail.com>
2015-10-27 15:29:09 -0400

    Add stubs to memberhsip_generator_spec

--- spec/models/service/membership_generator_spec.rb
+++ spec/models/service/membership_generator_spec.rb
@@ -1,19 +1,29 @@
 require 'spec_helper'
 
 describe MembershipGenerator do
-  describe "#generate_membership" do
-    it "creates membership for multiple users" do
-      users = create_list(:user, 3)
-      challenge = create(:challenge)
-      MembershipGenerator.new(challenge, users).generate
-      expect(Membership.count).to eq 3
-    end
 
-    it "creates membership for one user" do
-      user = create(:user)
-      challenge = create(:challenge)
-      MembershipGenerator.new(challenge, user).generate
-      expect(Membership.count).to eq 1
-    end
+  it "Should create a membership in the passed in challenge for all new_members passed in" do
+    fake_members = [double(:member1), double(:member2)]
+    challenge = double(:challenge, members: [])
+    allow(Membership).to receive(:create)
+    allow(MembershipCompletion).to receive(:new)
+
+    MembershipGenerator.new(challenge, fake_members).generate
+
+    expect(Membership).to have_received(:create).exactly(2).times
+    expect(MembershipCompletion).to have_received(:new).exactly(2).times
+  end
+
+
+  it "Should create a membership in the passed in challenge for all new_members passed in" do
+    challenge_member = double
+    challenge = double(:challenge, members: [challenge_member])
+    allow(Membership).to receive(:create)
+    allow(MembershipCompletion).to receive(:new)
+
+    MembershipGenerator.new(challenge, [challenge_member, double]).generate
+
+    expect(Membership).to have_received(:create).exactly(1).times
+    expect(MembershipCompletion).to have_received(:new).exactly(1).times
   end
 end
