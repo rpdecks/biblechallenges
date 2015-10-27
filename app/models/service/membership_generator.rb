5614b44bc3b4cd5417b8dea93c8755abc0a541af
Eric Chen <eric87chen@gmail.com>
2015-10-27 15:29:09 -0400

    Add stubs to memberhsip_generator_spec

--- app/models/service/membership_generator.rb
+++ app/models/service/membership_generator.rb
@@ -6,11 +6,7 @@ class MembershipGenerator
 
   def generate
     new_members_not_in_this_challenge.each do |new_member|
-      membership = Membership.new
-      membership.user = new_member 
-      membership.challenge = @challenge
-      membership.save
-
+      membership = Membership.create(user: new_member, challenge: @challenge)
       MembershipCompletion.new(membership)
     end
   end
