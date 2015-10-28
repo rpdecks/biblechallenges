d029ac93ce3ab8d97f62564b354a1ec792a483fe
Eric Chen <eric87chen@gmail.com>
2015-10-28 17:00:09 -0400

    WIP: refactor imports_members_from_previous_challenge_spec

--- spec/models/service/imports_members_from_previous_challenge_spec.rb
+++ spec/models/service/imports_members_from_previous_challenge_spec.rb
@@ -4,11 +4,14 @@ describe ImportsMembersFromPreviousChallenge do
   describe "#import" do
     it "imports members from previous challenge for creating a new challenge" do
       users = create_list(:user, 3)
-      old_challenge = create(:challenge, owner_id: users.first.id)
-      old_challenge.join_new_member(users)
-      new_challenge = create(:challenge, owner_id: users.first.id)
+      owner = double
+      challenge = build_stubbed(:challenge)
+      old_challenge = double(:old_challenge, id: challenge.id, owner: owner, members: users)
+      new_challenge = double(:new_challenge, owner: owner, members: [])
+      allow(Challenge).to receive(:find).and_return(old_challenge)
+
       ImportsMembersFromPreviousChallenge.new(old_challenge.id, new_challenge).import
-      new_challenge.reload
+
       expect(old_challenge.members).to match_array new_challenge.members
     end
   end
