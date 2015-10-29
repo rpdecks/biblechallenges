5bd229c04d72d236ebf7298c74a636d2148713c1
Evan Koch <evankoch@gmail.com>
2015-10-29 14:25:23 -0400

    FTer request for more anonymity on user/member name decorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -7,11 +7,11 @@ class UserDecorator < Draper::Decorator
     if name
       parts_of_name = name.split
       parts_of_name.each do |name_part|
-        # we want the entire last part of the name field
-        if name_part == parts_of_name.last
+        # we want the entire first part of the name field
+        if name_part == parts_of_name.first
           result += name_part
         else
-          result += "#{name_part[0]}. "
+          result += " #{name_part[0]}."
         end
       end
     end
