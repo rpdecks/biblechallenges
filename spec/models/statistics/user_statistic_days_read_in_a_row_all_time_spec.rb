2969d20c51eba4d60e7ffb303e51aa6b4d0e54e1
echen1031 <eric87chen@gmail.com>
2015-11-05 16:01:15 -0500

    Revert 1fd64e9ada459382edba17ab1891188b04792210

--- spec/models/statistics/user_statistic_days_read_in_a_row_all_time_spec.rb
+++ spec/models/statistics/user_statistic_days_read_in_a_row_all_time_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe UserStatisticDaysReadInARowAllTime do
   describe "#calculate" do
-    it "should calculate the proper value for user reading on consecutive days" do
+    it "should calculate the proper value for user reading on consecutive days, even after leaving challenge" do
       challenge = create(:challenge, :with_membership, chapters_to_read: 'Mark 1-7')
       user = challenge.members.first
       membership = challenge.memberships.first
@@ -11,8 +11,8 @@ describe UserStatisticDaysReadInARowAllTime do
       current_stat = user.user_statistics.find_by_type("UserStatisticDaysReadInARowCurrent")
       all_time_stat = user.user_statistics.find_by_type("UserStatisticDaysReadInARowAllTime")
 
-      challenge.readings[0..4].each do |mr|
-        create(:membership_reading, membership: membership, reading: mr)
+      challenge.readings[0..4].each do |r|
+        create(:membership_reading, membership: membership, reading: r, user_id: user.id)
         Timecop.travel(1.day)
       end
       current_stat.update
@@ -20,13 +20,14 @@ describe UserStatisticDaysReadInARowAllTime do
 
       Timecop.travel(1.days) # skip one day
 
-      challenge.readings[5..6].each do |mr|
-        create(:membership_reading, membership: membership, reading: mr)
+      challenge.readings[5..6].each do |r|
+        create(:membership_reading, membership: membership, reading: r, user_id: user.id)
         Timecop.travel(1.day)
       end
       current_stat.update
       all_time_stat.update
 
+      membership.destroy #user leaves challenge
       expect(current_stat.value).to eq 2
       expect(all_time_stat.value).to eq 5
       Timecop.return
