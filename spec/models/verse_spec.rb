f8dc72a04b7dfa2df63b20cfb07e44fa697a3562
Evan Koch <evankoch@gmail.com>
2016-02-18 14:00:46 -0500

    refactor RetrieveRcv & move by_version out of Verse to Chapter model

--- spec/models/verse_spec.rb
+++ spec/models/verse_spec.rb
@@ -12,44 +12,6 @@ describe Verse do
   end
 
   describe "scopes" do
-    describe "by_version" do
-      it "returns a specified version when that version exists" do
-        matthew1 = Chapter.find_by_book_id_and_chapter_number(40, 1)
-        verses = matthew1.verses.by_version("NASB")
-
-        expect(verses.first.version).to eq "NASB"
-      end
-      it "returns specified RCV version (after adding verse object/text) " do
-        matthew1 = Chapter.find_by_book_id_and_chapter_number(40, 1)
-          # this 'check' also populates the nonexistant RCV text
-        rcv_verses = matthew1.verses.by_version("RCV")
-
-        expect(rcv_verses.first.version).to eq "RCV"
-      end
-      it "Does not update/replace the RCV verse text if verse_text not updated within past 10 days" do
-        psalm23 = Chapter.find_by_book_id_and_chapter_number(19, 23)
-        psalm23.verses.by_version("RCV") #initially populates db with RCV verse_text
-        Timecop.travel(9.days)
-
-        expect(psalm23.verses.by_version("RCV").first.updated_at.to_date).to eq 9.days.ago.to_date
-        Timecop.return
-      end
-      it "Updates/replaces the RCV verse text if verse_text not updated within past 10 days" do
-        psalm23 = Chapter.find_by_book_id_and_chapter_number(19, 23)
-        psalm23.verses.by_version("RCV") #initially populates db with RCV verse_text
-        Timecop.travel(11.days)
-        psalm23.verses.by_version("RCV")
-        expect(psalm23.verses.first.updated_at.to_date).to eq Time.zone.now.to_date
-        Timecop.return
-      end
-
-      it "returns the default version when the specified version does not exist" do
-        #how do I put this default version into a constant somewhere?
-        matthew1 = Chapter.find_by_book_id_and_chapter_number(40, 1)
-        verses = matthew1.verses.by_version("IMAGINARY")
-        expect(verses.first.version).to eq "ASV"
-      end
-    end
 
     describe "by_range" do
       chapter = Chapter.find_by_book_name_and_chapter_number("Genesis", 1)
