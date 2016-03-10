b6f3cfdae22d29d043548d2b0ecb7986806bbcb9
Jebin (Beyond) <jebin@beyondfinity.com>
2016-03-10 15:18:34 +0530

    Added scheduler and daily task staus email and optimized config settings

---
+++ app/mailers/task_stat.rb
@@ -0,0 +1,17 @@
+class TaskStat < ApplicationMailer
+
+	def daily_email_stat(challenges_count,members_count,emails_count,t_errors,s_time,e_time,exec_time)
+		@total_challenges = challenges_count
+		@total_members = members_count
+		@emails_count = emails_count
+		@errors = t_errors
+		@start_time = s_time
+		@end_time = e_time
+		@execution_time = exec_time
+		@date = Time.current.to_date.strftime("%b %d, %Y")
+		mail(
+		  to: "bc-trial-email@beyondfinity.com",
+		  subject: "DailyEmailScheduler Report - #{@date}"
+		)
+	end
+end
