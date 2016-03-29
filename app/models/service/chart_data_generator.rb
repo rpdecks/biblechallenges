ebc9d238c00b434347f9bbb4d5eeecc3f47071ac
Evan Koch <evankoch@gmail.com>
2016-03-29 17:11:46 -0400

    WIP: need to test for member_readings_data

--- app/models/service/chart_data_generator.rb
+++ app/models/service/chart_data_generator.rb
@@ -1,32 +1,42 @@
 class ChartDataGenerator
+  STARTING_DATA = ["start", 0]
+
   def initialize(readings:, membership_readings:)
     @readings = readings
     @membership_readings = membership_readings
   end
 
   def benchmark_data
-    initial_setup
+    benchmark_initial_setup
     @readings.pluck(:read_on).uniq.each do |date|
-      @benchmark_data << [
+      @benchmark_data_array << [
                           date.strftime("%-m/%-e"),
                           @readings.select { |x| x.read_on.to_date == date.to_date }.size +
-                          @benchmark_data.last[-1]
+                          @benchmark_data_array.last[-1]
                          ]
     end
-    @benchmark_data
+    @benchmark_data_array
   end
 
-  def whole_challenge_membership_reading_data
-    membership_readings_data = [["start", 0]]
+  def member_reading_data
+    member_readings_initial_setup
     @readings.pluck(:read_on).uniq.each do |r|
-      membership_readings_data << [r.strftime("%-m/%-e"), (@membership_readings.select { |x| x.created_at.to_date == r.to_date }.size) + membership_readings_data.last[-1]]
+      @member_readings_data_array << [
+                                      r.strftime("%-m/%-e"),
+                                      (@membership_readings_data_array.select { |x| x.created_at.to_date == r.to_date }
+                                      .size) + @member_readings_data_array.last[-1]
+                                     ]
     end
-
-    membership_readings_data
   end
 
-  def initial_setup
-    @benchmark_data = [["start", 0]]
+  def member_readings_initial_setup
+    @member_readings_data_array = []
+    @member_readings_data_array << STARTING_DATA
+  end
+
+  def benchmark_initial_setup
+    @benchmark_data_array = []
+    @benchmark_data_array << STARTING_DATA
     @chapters_per_day = 1
     @date_string_for_shovel = @readings.first.read_on.strftime("%-m/%-e")
   end
