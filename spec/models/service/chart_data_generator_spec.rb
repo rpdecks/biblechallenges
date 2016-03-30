8f26a961c9abe0a281cfb4268c6e202875013ade
Evan Koch <evankoch@gmail.com>
2016-03-30 15:21:08 -0400

    update create(:challenge) in tests to reflect needed readings for member_challenges_controller actions

--- spec/models/service/chart_data_generator_spec.rb
+++ spec/models/service/chart_data_generator_spec.rb
@@ -8,7 +8,24 @@ describe ChartDataGenerator do
       membership_readings = double("membership_readings")
       benchmark_data = ChartDataGenerator.new(readings: readings, membership_readings: membership_readings).benchmark_data
 
+      expect(benchmark_data.first).to eq ["start", 0]
       expect(benchmark_data.count).to eq (readings.count + 1)
+    end
+  end
+  describe "member_reading_data" do
+    it "returns an array of membership readings data" do
+      challenge = create(:challenge, :with_readings)
+      membership = create(:membership, challenge: challenge)
+      readings = challenge.readings
+      create(:membership_reading, membership: membership)
+      create(:membership_reading, membership: membership)
+      mrs = membership.membership_readings
+
+      member_data = ChartDataGenerator.new(readings: readings,
+                                           membership_readings: mrs).member_reading_data
+
+      expect(member_data.count).to eq (readings.count + 1)
+      expect(member_data.first).to eq ["start", 0]
     end
   end
 end
