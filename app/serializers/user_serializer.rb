c7a6bb5b23c1b735634a84d8dafbdd3d354d61c0
Rathan (Beyond) <rathan@beyondfinity.com>
2016-05-26 18:28:32 +0530

    add hardcoded path for default avatar image since image_url helper is not returning correct asset path

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -5,13 +5,12 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :avatar_path
 
   def avatar_path
-    #'/assets/default_avatar.png'
     user = object
 
     if user && user.avatar_file_name
       user.avatar.url(:thumb)
     else
-      user.image || image_url('default_avatar.png')
+      user.image || '/assets/default_avatar.png'
     end
   end
 end
