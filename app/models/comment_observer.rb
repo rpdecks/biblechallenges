4a3d16b43c655f3baf176ca26d665e70b35316f9
Rathan (Beyond) <rathan@beyondfinity.com>
2016-05-27 17:43:55 +0530

    revert temporary changes previously made for testing

--- app/models/comment_observer.rb
+++ app/models/comment_observer.rb
@@ -2,7 +2,7 @@ class CommentObserver < ActiveRecord::Observer
 
   def after_create(comment)
     if (comment.commentable.is_a? Comment) && (comment.commentable.user.comment_notify == true)
-      #CommentMailer.new_comment_notification(comment).deliver_now
+      CommentMailer.new_comment_notification(comment).deliver_now
     end
   end
 
