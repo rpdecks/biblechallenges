433a2cd02c241d9d242b5a4260c29aab2dac382f
Philip Bradley <pdbradley@gmail.com>
2016-08-10 08:34:44 -0400

    add prettier helper for the comment div to drop into views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,4 +29,31 @@ module ApplicationHelper
     [["ASV (American Standard)",'ASV'],["ESV (English Standard)",'ESV'],["KJV (King James)",'KJV'],["NASB (New American Standard)",'NASB'],["NKJV (New King James)",'NKJV'],["RCV (Recovery Version)", 'RCV']]
   end
 
+  def react_chat(commentable, user = current_user)
+    commentable_type = commentable.class.name
+    react_component("CommentList", {
+      commentableType: commentable_type, 
+      commentableId: commentable.id, 
+      comments: comments_json(commentable), 
+      currentUser: current_user_json(user)
+    })
+  end
+
+# I know this is gross but I'm leaving these helper examples in here; they are helpful
+# raw [
+#   tag('meta', property: 'og:image', content: image_url('ralph-gradient.png')),
+#   tag('meta', property: 'og:url', content: url_for(only_path: false)),
+#   tag('meta', property: 'og:title', content: page_title),
+# ].join("\n")
+
+# def trail_breadcrumbs(trail, separator = ">")
+#   links = [
+#     link_to("Trails", practice_path),
+#     trail.topics.map { |topic| link_to(topic, topic) },
+#     link_to(trail, trail),
+#   ].flatten
+
+#   links.join(" #{separator} ").html_safe
+# end
+
 end
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,13 +1,14 @@
 module CommentsHelper
-	def comments_json(commentable_object)
-		if commentable_object
-			eval(ActiveModel::ArraySerializer.new(commentable_object.comments.recent_last, each_serializer: CommentSerializer).to_json)
-		end
-	end
+  def comments_json(commentable_object)
+    if commentable_object
+      eval(ActiveModel::ArraySerializer.new(commentable_object.comments.recent_last,
+                                            each_serializer: CommentSerializer).to_json)
+    end
+  end
 
-	def current_user_json
-		if current_user
-			eval(UserSerializer.new(current_user).to_json)
-		end
-	end
+  def current_user_json(user = current_user)
+    if user
+      eval(UserSerializer.new(user).to_json)
+    end
+  end
 end
