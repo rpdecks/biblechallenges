1473575203548bbb4e15c5012ec8d7b4c09630e9
Eric Chen <eric87chen@gmail.com>
2016-08-24 11:04:25 -0400

    Removed unnecessary code

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -14,12 +14,4 @@ class Users::SessionsController < Devise::SessionsController
   def after_sign_out_path_for(resource)
     session[:previous_url] = ""
   end
-
-  def user_params
-    params[:user]
-  end
-
-  def user_already_has_a_google_account
-    @user && @user.provider == "google_oauth2"
-  end
 end
